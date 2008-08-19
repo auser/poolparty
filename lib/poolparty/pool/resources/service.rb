db75247c4d727d56cb10b93f4935c9b99b8e0d0e
Ari Lerner <ari.lerner@citrusbyte.com>
2008-08-18 18:57:11 -0700

    Updated and aded services

--- lib/poolparty/pool/resources/service.rb
+++ lib/poolparty/pool/resources/service.rb
@@ -1,7 +1,21 @@
 module PoolParty    
   module Resources
     
+    def service(opts={}, &block)
+      returning PoolParty::Resources::Service.new(opts, &block) do |r|
+        resources << r
+      end
+    end
+    
     class Service < Resource
+      
+      default_options({
+        :besure => "running",
+        :enable => "true",
+        :name => nil,
+        :require => nil
+      })      
+      
     end
     
   end
--- spec/pool/resources/service_spec.rb
+++ spec/pool/resources/service_spec.rb
@@ -1,22 +1,43 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
+include PoolParty::Resources
+
 describe "Service" do
   before(:each) do
     @service = PoolParty::Resources::Service.new
   end
-  it "should have instances of files" do
+  it "should have instances of Services" do
     @service.respond_to?(:instances).should == true
   end
   describe "instances" do
     before(:each) do
-      @service << {:name => "puppetmaster"}
+      @instance1 = {:name => "/etc/apache2/puppetmaster.conf"}
+      @service << @instance1
     end
     it "should turn the one hash instance into a string" do
-      @service.to_s.should == "service {\n\tpuppetmaster:\n\t\tname => puppetmaster;\n}"
+      @service.to_s.should =~ /name => \/etc\/apache2\/puppetmaster\.conf;/      
     end
     it "should turn the two hash instance into a string" do
-      @service << {:name => "apache"}
-      @service.to_s.should == "service {\n\tpuppetmaster:\n\t\tname => puppetmaster;\n\tapache:\n\t\tname => apache;\n}"
+      @instance2 = {:name => "/etc/init.d/puppetmaster"}
+      @service << @instance2
+      @service.to_s.should =~ /name => \/etc\/apache2\/puppetmaster\.conf;/
+    end
+    describe "as included" do            
+      before(:each) do
+        @service = service({:rent => "low"}) do
+          name "mdmdp"
+          besure "running"
+        end
+      end
+      it "should use default values" do
+        @service.name.should == "mdmdp"
+      end
+      it "should keep the default values for the Service" do
+        @service.enable.should == "true"
+      end
+      it "should also set options through a hash" do
+        @service.rent.should == "low"
+      end
     end
   end
 end
