b720fd822721e956c64edaba06deac8ed010f4b1
Ari Lerner <ari.lerner@citrusbyte.com>
2008-08-31 16:06:11 -0700

    Changed the spec template

--- generators/poolspec/poolspec_generator.rb
+++ generators/poolspec/poolspec_generator.rb
@@ -21,15 +21,12 @@ class PoolspecGenerator < RubiGen::Base
       m.directory ''
       BASEDIRS.each { |path| m.directory path }
       
-      m.template "pool_spec_template.erb", "poolspec.rb"
+      m.template "pool_spec_template.erb", "config/poolspec.rb"
       # Create stubs
       # m.template "template.rb",  "some_file_after_erb.rb"
       # m.template_copy_each ["template.rb", "template2.rb"]
       # m.file     "file",         "some_file_copied"
       # m.file_copy_each ["path/to/file", "path/to/file2"]
-
-      m.dependency "install_rubigen_scripts", [destination_root, 'poolspec'],
-        :shebang => options[:shebang], :collision => :force
     end
   end
 
