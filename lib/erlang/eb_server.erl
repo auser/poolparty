d4cbcdf144840a800cce6067844ff78088d8a6c6
Ari Lerner <ari.lerner@citrusbyte.com>
2008-09-30 12:06:04 -0700

    Adding in basic erlang server to handle the load monitoring Updated the poolparty base package to include erlang as a package

---
+++ lib/erlang/eb_server.erl
@@ -0,0 +1,9 @@
+%%%***************************************
+%%% 
+%%% PoolParty node-server
+%%% Author: Ari Lerner <ari.the.lerner@gmail.com>
+%%% 
+%%% Description: This server runs on the poolparty nodes
+%%% 
+%%%***************************************
+
