c0c4ef5772f76bf3bcc83ad4bcbd578b43c47bce
Ari Lerner <ari.lerner@citrusbyte.com>
2008-10-13 03:06:13 -0700

    Updated gem package to assume parent options

--- lib/poolparty/plugins/gem_package.rb
+++ lib/poolparty/plugins/gem_package.rb
@@ -5,8 +5,8 @@ module PoolParty
       
       def loaded
         has_exec(:name => "gem-package-#{name}", :cwd => "/tmp", :path => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/var/lib/gems/1.8/bin") do
-          command "gem install --no-ri --no-rdoc #{"--version \"#{version}\"" if respond_to?(:version)} #{"--source \"#{source}\"" if respond_to?(:source)} #{@parent.name}"
-          ifnot "gem list --local #{@parent.name} | grep #{@parent.name} #{"| grep #{version}" if respond_to?(:version)}"
+          command "gem install --no-ri --no-rdoc #{"--version \"#{version}\"" if @parent.version} #{"--source #{@parent.source}" if @parent.source} #{@parent.name}"
+          ifnot "gem list --local #{@parent.name} | grep #{@parent.name} #{"| grep #{@parent.version}" if @parent.version}"
         end
       end
       
