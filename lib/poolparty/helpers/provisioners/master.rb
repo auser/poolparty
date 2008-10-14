cbba076382446a4479d943c2e012f3b847b08a18
Ari Lerner <ari.lerner@citrusbyte.com>
2008-10-14 01:32:38 -0700

    Update to how the puppet server starts

--- lib/poolparty/helpers/provisioners/master.rb
+++ lib/poolparty/helpers/provisioners/master.rb
@@ -137,17 +137,17 @@ mv #{Base.remote_storage_path}/#{Base.tmp_path}/#{@cloud.full_keypair_name} #{@c
         EOS
       end
 
+      # ps aux | grep puppetmasterd | awk '{print $2}' | xargs kill
+      # rm -rf /etc/puppet/ssl
       def start_puppetmaster
         <<-EOS
-ps aux | grep puppetmasterd | awk '{print $2}' | xargs kill
-rm -rf /etc/puppet/ssl
 puppetmasterd
         EOS
       end
 
       def restart_puppetd
         <<-EOS
-          puppetd --listen
+          puppetd --listen --fqdn=#{@cloud.master.name}
         EOS
       end
     end
