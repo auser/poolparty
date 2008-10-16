374de0dbe7031b4b41bbdec8c1478e2d9a2fa714
Ari Lerner <ari.lerner@citrusbyte.com>
2008-10-16 01:36:05 -0700

    Changed the server-list-active method to remove the array return

--- lib/poolparty/base_packages/heartbeat.rb
+++ lib/poolparty/base_packages/heartbeat.rb
@@ -12,8 +12,8 @@ module PoolParty
           has_exec(:name => "heartbeat-update-cib", :command => "/usr/sbin/cibadmin -R -x /etc/ha.d/cib.xml", :refreshonly => true)
         
           # variables for the templates
-          variable(:name => "ha_nodenames", :value => 'generate(". /etc/profile && server-list-active -c name")')
-          variable(:name => "ha_node_ips",  :value => 'generate(". /etc/profile && server-list-active -c ip")')
+          variable(:name => "ha_nodenames", :value => "generate('/usr/bin/env', '/var/lib/gems/1.8/bin/server-list-active', '-c', 'name')")
+          variable(:name => "ha_node_ips",  :value => "generate('/usr/bin/env', '/var/lib/gems/1.8/bin/server-list-active', '-c', 'ip')")
           
           has_variable({:name => "ha_timeout",  :value => (self.respond_to?(:timeout) ? timeout : "5s")})
           has_variable({:name => "ha_port", :value => (self.respond_to?(:port) ? port : Base.port)})
