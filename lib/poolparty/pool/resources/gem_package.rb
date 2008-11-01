66be5759e368ed4a0c004e1fa7156d53288f8f14
Ari Lerner <ari.lerner@citrusbyte.com>
2008-10-31 17:50:44 -0700

    Updated gem-package, moving the master tasks

--- lib/poolparty/pool/resources/gem_package.rb
+++ lib/poolparty/pool/resources/gem_package.rb
@@ -12,15 +12,15 @@ module PoolParty
         if download_url
           
           has_file(:name => "#{Base.remote_storage_path}/#{name}.gem", :source => "#{Base.fileserver_base}/#{name}.gem")
-          
-          execute_if("$hostname", "master") do
-            has_exec(:name => "download-#{name}", :cwd => Base.remote_storage_path, :command => "wget #{download_url} -O #{name}.gem", :ifnot => "test -f #{Base.remote_storage_path}/#{name}.gem", :notify => get_file("#{Base.remote_storage_path}/#{name}.gem"))
-          end          
-          
+                    
           has_exec(opts.merge({:name => "#{name}", :cwd =>"#{Base.remote_storage_path}", :path => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/var/lib/gems/1.8/bin"})) do
             command "gem install -y --no-ri --no-rdoc #{Base.remote_storage_path}/#{name}.gem"
             ifnot "gem list --local #{name} | grep #{name} #{"| grep #{version}" if version}"
             requires get_file("#{Base.remote_storage_path}/#{name}.gem")
+          end
+          
+          execute_if("$hostname", "master") do
+            has_exec(:name => "download-#{name}", :cwd => Base.remote_storage_path, :command => "wget #{download_url} -O #{name}.gem", :ifnot => "test -f #{Base.remote_storage_path}/#{name}.gem")
           end
         else
           has_exec(opts.merge({:name => "#{name}", :cwd => "/tmp", :path => "/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/var/lib/gems/1.8/bin"})) do
