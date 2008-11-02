f5af9a63bc414bcac6ff3aaf3b308797b9c9c232
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-02 00:30:50 -0700

    Updated provisioner base

--- lib/erlang/cloudpanel/src/cloudpanel_web.erl
+++ lib/erlang/cloudpanel/src/cloudpanel_web.erl
@@ -1,7 +1,7 @@
-%% @author author <author@example.com>
-%% @copyright YYYY author.
+%% @author Ari Lerner <ari.lerner@citrusbyte.com>
+%% @copyright 2008 Ari Lerner.
 
-%% @doc Web server for chat.
+%% @doc PoolParty panel
 
 -module (cloudpanel_web).
 -author('Ari Lerner <ari.lerner@citrusbyte.com>').
@@ -9,137 +9,33 @@
 -export([start/1, stop/0, loop/2]).
 
 -define(TIMEOUT, 20000).
+-define (PORT, 8001).
 
 %% External API
 
 start(Options) ->
     {DocRoot, Options1} = get_option(docroot, Options),
     Loop = fun (Req) ->
-                   ?MODULE:loop(Req, DocRoot)
+						?MODULE:loop(Req, DocRoot)
            end,
-    mochiweb_http:start([{max, 1000000}, {name, ?MODULE}, {loop, Loop} | Options1]).
+    mochiweb_http:start([{max, 1000000}, {name, ?MODULE}, {loop, Loop}, {port, PORT} | Options1]).
 
 stop() ->
     mochiweb_http:stop(?MODULE).
 
-room(Users) ->
-    receive
-        {From, subscribe} ->
-            From ! subscribed,
-            room([From | Users]);
-        {From, unsubscribe} ->
-            From ! unsubscribed,
-            room(Users -- [From]);
-        {From, post, Message} ->
-            From ! posted,
-            lists:foreach(fun(User) ->
-                    % broadcast the message
-                    User ! Message
-                end, Users),
-            room([]);
-        _Any ->
-            room(Users)
-    end.
-
-get_the_room() ->
-    % does the room exists?
-    Pid = whereis(theroom),
-    if
-        is_pid(Pid) ->
-            % yup
-            Pid;
-        true ->
-            % create it
-            NewPid = spawn(fun() ->
-                room([])
-            end),
-            register(theroom, NewPid),
-            NewPid
-    end.
-
 loop(Req, DocRoot) ->
-    "/" ++ Path = Req:get(path),
-    case Req:get(method) of
-        Method when Method =:= 'GET'; Method =:= 'HEAD' ->
-            case Path of
-                "chat" ->
-                    Room = get_the_room(),
-                    Room ! {self(), subscribe},
-                    receive
-                        subscribed ->
-                            % subscription is ok
-                            % now wait for a message
-                            receive
-                                Message ->
-                                    {Type, Message} = {ok, Message}
-                            after ?TIMEOUT ->
-                                % we waited too long
-                                {Type, Message} = {error, <<"timeout">>}
-                            end
-                    after 1000 ->
-                        % subscription failed on time
-                        {Type, Message} = {error, <<"timeout">>}
-                    end,
+	ok.
 
-                    case Type of
-                        error ->
-                            % we need to unsubscribe from the room
-                            % because it failed somewhere
-                            Room ! {self(), unsubscribe},
-                            receive
-                                unsubscribed ->
-                                    % unsubscribed
-                                    ok
-                            after 1000 ->
-                                % do not wait too long
-                                ok
-                            end;
-                        ok ->
-                            % everything went fine
-                            ok
-                    end,
-                    
-                    % send back the JSON message
-                    Req:ok({"text/javascript", mochijson2:encode({
-                            struct, [
-                                {Type, Message}
-                            ]
-                        })
-                    });
-                _ ->
-                    Req:serve_file(Path, DocRoot)
-            end;
-        'POST' ->
-            case Path of
-                "chat" ->
-                    Data = Req:parse_post(),
-                    Room = get_the_room(),
-                    % post
-                    Room ! {self(), post, list_to_binary(proplists:get_value("message", Data))},
-                    receive
-                        posted ->
-                            % posted
-                            Body = {ok, <<"posted">>}
-                    after 1000 ->
-                        % something went wrong
-                        Body = {error, <<"timeout">>}
-                    end,
-                    
-                    % send back the JSON message
-                    Req:ok({"text/javascript", mochijson2:encode({
-                            struct, [
-                                Body
-                            ]
-                        })
-                    });
-                _ ->
-                    Req:not_found()
-            end;
-        _ ->
-            Req:respond({501, [], []})
-    end.
+subst(Template, Values) when is_list(Values) ->
+	erlang:list_to_binary(lists:flatten(io_lib:fwrite(Template, Values))).
 
-%% Internal API
+clean_path(Path) ->
+	case string:str(Path, "?") of
+		0 ->
+			Path;
+		N ->
+			string:substr(Path, 1, string:len(Path) - (N + 1))
+	end.
 
 get_option(Option, Options) ->
     {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
