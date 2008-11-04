689bb6506d98f0475e7d25d8f3029a4193f8302e
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-03 17:59:05 -0800

    Starting logger in cloud pool

--- lib/erlang/cloudpanel/src/cloudpanel_web.erl
+++ lib/erlang/cloudpanel/src/cloudpanel_web.erl
@@ -8,13 +8,12 @@
 
 -import(random).
 -export([start/1, stop/0, loop/2]).
-
--record(doc, {id, name, body}).
+-include ("cloudpanel.hrl").
 
 %% External API
 
 start(Options) ->
-    {DocRoot, Options1} = get_option(docroot, Options),
+    {DocRoot, Options1} = utils:get_option(docroot, Options),
     Loop = fun (Req) ->
                    ?MODULE:loop(Req, DocRoot)
            end,
@@ -23,39 +22,18 @@ start(Options) ->
 stop() ->
     mochiweb_http:stop(?MODULE).
 
-loop(Req, DocRoot) ->
-    "/" ++ Path = Req:get(path),
-    case Req:get(method) of
-        Method when Method =:= 'GET'; Method =:= 'HEAD' ->
-            case Path of
-                _ ->
-									wrap(read_page("index"))
-                    % Req:serve_file(Path, DocRoot)
-            end;
-        'POST' ->
-            case Path of
-                _ ->
-                    Req:not_found()
-            end;
-        _ ->
-            Req:respond({501, [], []})
+loop(Req, _DocRoot) ->
+		Path = Req:get(path),
+		case {Path, Req:get(method)} of
+			{"/tail", 'GET'} ->
+				Body = tailor:go(Req:parse_qs()),
+				Req:ok({"text/html", [], Body});
+			{"/", 'GET'} ->
+				Body = views:wrap_page("index"),
+				Req:ok({"text/html", [], Body});
+			{_, 'GET'} ->
+				Body = views:wrap_page(Path),
+				Req:ok({"text/html", [], Body});
+       _ ->
+        Req:respond({501, [], []})
     end.
-
-%% Internal API
-
-wrap(Content) ->
-	Header = read_partial("header"),
-	Menu = read_partial("menu"),
-	Footer = read_partial("footer"),
-	Out = erlang:list_to_binary([Header, Menu, Content, Footer]),
-	erlang:binary_to_list(Out).
-
-read_page(Name) ->
-	{_, Cont} = file:read_file("priv/www/pages/"++Name++".html"),
-	Cont.
-read_partial(Name) ->
-	{_, Cont} = file:read_file("priv/www/partials/"++Name++".html"),
-	Cont.
-	
-get_option(Option, Options) ->
-    {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
