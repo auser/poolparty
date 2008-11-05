427af2e892524e3e9fbfb9c7dd6abe1c6eb9fee9
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-05 02:29:56 -0800

    Adding mount points

---
+++ lib/poolparty/modules/cloud_dsl.rb
@@ -0,0 +1,12 @@
+module PoolParty
+  module CloudDsl
+    
+    def mount_ebs_volume_at(id="", loc="/data")
+      ebs_volume_id id
+      ebs_volume_mount_point loc
+      has_directory(:name => loc)
+      has_mount(:name => loc)
+    end
+    
+  end
+end
