fb826a2426cb365df2bc28ffcda442f8a2724c93
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-05 15:05:33 -0800

    Updated generator

--- generators/poolspec/poolspec_generator.rb
+++ generators/poolspec/poolspec_generator.rb
@@ -21,7 +21,7 @@ class PoolspecGenerator < RubiGen::Base
       m.directory ''
       BASEDIRS.each { |path| m.directory path }
       
-      m.template "pool_spec_template.erb", "#{@name}.rb"
+      m.template "pool_spec_template.erb", "#{@name}.spec"
       # Create stubs
       # m.template "template.rb",  "some_file_after_erb.rb"
       # m.template_copy_each ["template.rb", "template2.rb"]
@@ -61,5 +61,6 @@ EOS
     # Installation skeleton.  Intermediate directories are automatically
     # created so don't sweat their absence here.
     BASEDIRS = %w(
+      plugins
     )
 end
