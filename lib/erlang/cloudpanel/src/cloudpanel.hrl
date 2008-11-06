2c8c8a577937c26170f2279d5ce858689ae7979c
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-06 15:14:05 -0800

    Removed cloupanel entirely from the repos

--- lib/erlang/cloudpanel/src/cloudpanel.hrl
+++
@@ -1,2 +0,0 @@
-
--define (TIMEOUT, 1000).
--- lib/erlang/cloudpanel/src/utils.erl
+++
@@ -1,9 +0,0 @@
--module (utils).
--compile(export_all).
-
-
-subst(Template, Values) when is_list(Values) ->
-    list_to_binary(lists:flatten(io_lib:fwrite(Template, Values))).
-
-get_option(Option, Options) ->
-    {proplists:get_value(Option, Options), proplists:delete(Option, Options)}.
