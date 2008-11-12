d9dc5c65011245c1cfcbdb3a07bc6e4dc30d3266
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-11 20:33:10 -0800

    Updated messenger

---
+++ lib/erlang/messenger/src/pm_strings.erl
@@ -0,0 +1,11 @@
+-module (pm_strings).
+-include_lib("../include/defines.hrl").
+-compile(export_all).
+
+string_join(Items, Sep) ->
+    lists:flatten(lists:reverse(string_join1(Items, Sep, []))).
+
+string_join1([Head | []], _Sep, Acc) ->
+    [Head | Acc];
+string_join1([Head | Tail], Sep, Acc) ->
+    string_join1(Tail, Sep, [Sep, Head | Acc]).
