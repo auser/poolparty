daaa4beea4d22427bd229bfd87663ff32fbc5a1f
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-12 13:36:10 -0800

    Updated messenger to remove the debugging lines, the provisioning to fully qualify the paths

--- lib/poolparty/helpers/provisioner_base.rb
+++ lib/poolparty/helpers/provisioner_base.rb
@@ -100,7 +100,7 @@ module PoolParty
           process_clean_reconfigure_for!(@instance, testing)
           
           vputs "Logging in and running provisioning on #{@instance.name}"
-          cmd = "cd #{Base.remote_storage_path} && chmod +x install_#{name}.sh && /bin/bash install_#{name}.sh; rm install_#{name}.sh"
+          cmd = "cd #{Base.remote_storage_path} && /bin/chmod +x install_#{name}.sh && /bin/bash install_#{name}.sh; /bin/rm install_#{name}.sh"
           verbose ? @cloud.run_command_on(cmd, @instance) : hide_output {@cloud.run_command_on(cmd, @instance)}
           
           process_clean_reconfigure_for!(@instance, testing)
@@ -134,7 +134,7 @@ module PoolParty
           vputs "Logging on to #{@instance.ip}"
           @cloud.rsync_storage_files_to(@instance)
 
-          cmd = "cd #{Base.remote_storage_path} && chmod +x configure_#{name}.sh && /bin/bash configure_#{name}.sh && rm configure_#{name}.sh"
+          cmd = "cd #{Base.remote_storage_path} && /bin/chmod +x configure_#{name}.sh && /bin/bash configure_#{name}.sh && /bin/rm configure_#{name}.sh"
           verbose ? @cloud.run_command_on(cmd, @instance) : hide_output {@cloud.run_command_on(cmd, @instance)}
         end
       end
