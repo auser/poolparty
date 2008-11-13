1cf6e648688daa05daae2206a2c51501d66ccc6e
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-13 05:16:06 -0800

    Updated the searching for the pool.spec as well as added deploydirectory and updated the gem for github

---
+++ lib/poolparty/plugins/deploydirectory.rb
@@ -0,0 +1,48 @@
+module PoolParty    
+  class Deploydirectory
+        
+    virtual_resource(:deploydirectory) do
+      
+      def loaded(opts={}, parent=self)
+        package_directory
+        unpack_directory
+      end
+            
+      def package_directory
+        path = ::File.join( Base.tmp_path, "#{::File.basename(from_dir)}.tar.gz" )
+        # cd /Users/auser/Sites/work/citrusbyte/internal/gems/pool-party/poolparty/spec/poolparty/plugins/ && tar -czf plugins.tar.gz . && mv plugins.tar.gz /tmp/poolparty && cd /tmp/poolparty
+        cmd = "cd #{::File.expand_path(from_dir)} && tar -czf #{::File.basename(from_dir)}.tar.gz . && mv #{::File.basename(from_dir)}.tar.gz #{Base.tmp_path}"
+        `#{cmd}` unless testing
+      end
+      
+      def unpack_directory         
+        has_exec({:name => "deploy-directory-#{name}", :requires => get_directory("#{cwd}"), :cwd => cwd}) do
+          command "cd #{parent.cwd}; tar -zxf #{Base.tmp_path}/#{parent.name}.tar.gz"
+        end
+      end
+      
+      def from(dir)
+        from_dir dir
+      end
+      
+      def to(dir)
+        cwd dir
+        has_directory(:name => "#{dir}", :requires => get_directory("#{::File.dirname(dir)}"))
+      end
+      
+      # Since git is not a native type, we have to say which core resource
+      # it is using to be able to require it
+      def class_type_name
+        "exec"
+      end
+      
+      # Because we are requiring an exec, instead of a built-in package of the git, we have to overload
+      # the to_s method and prepend it with the same name as above
+      def key
+        "deploy-directory-#{name}"
+      end
+      
+    end
+    
+  end
+end
---
+++ spec/poolparty/plugins/deploydirectory_spec.rb
@@ -0,0 +1,44 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+include PoolParty::Resources
+
+class TestClass
+  include PoolParty::Resources
+end
+describe "Remote Instance" do
+  before(:each) do
+    reset_resources!
+  end
+  describe "wrapped" do
+    before(:each) do
+      @tc = TestClass.new
+      @options = {:name => "deploydirectory", :from => ::File.dirname(__FILE__), :to => "/var/www/deploydirectory", :testing => false}
+    end
+    it "should be a string" do
+      @tc.has_deploydirectory(@options).to_string.should =~ /exec \{/
+    end
+    it "should included the flushed out options" do
+      @tc.has_deploydirectory(@options).to_string.should =~ /command/
+    end
+    describe "in resource" do
+      before(:each) do
+        @tc.instance_eval do
+          has_deploydirectory do
+            name "deploydirectory"
+            from ::File.dirname(__FILE__)
+            to "/var/www/deploydirectory"
+          end
+        end
+      end
+      it "should have the path set within the resource" do
+        @tc.resource(:deploydirectory).first.to_string.should =~ /exec \{/
+      end
+      it "should not have the from in the to_string" do
+        @tc.resource(:deploydirectory).first.to_string.should_not =~ /from /
+      end
+      it "should not have the to in the to_string" do
+        @tc.resource(:deploydirectory).first.to_string.should_not =~ /to /
+      end
+    end
+  end
+end
