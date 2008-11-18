97607903356165f845f0a486de7a3e05463d36a3
Ari Lerner <ari.lerner@citrusbyte.com>
2008-11-18 13:25:44 -0800

    Added ec2 deconstruction for the time being

--- lib/poolparty/net/remote_bases/ec2.rb
+++ lib/poolparty/net/remote_bases/ec2.rb
@@ -1,6 +1,8 @@
 begin
   require 'EC2'
   require "date"
+  require "#{::File.dirname(__FILE__)}/ec2/ec2_response_object"
+  
   class String
     def convert_from_ec2_to_ip
       self.gsub(/.compute-1.amazonaws.com*/, '').gsub(/ec2-/, '').gsub(/-/, '.')
@@ -124,60 +126,6 @@ begin
       end
     end
     register_remote_base :Ec2
-  end
-
-  # Provides a simple class to wrap around the amazon responses
-  class EC2ResponseObject
-    def self.get_descriptions(resp)      
-      rs = get_response_from(resp)
-
-      # puts rs.methods.sort - rs.ancestors.methods
-      out = begin
-        if rs.respond_to?(:instancesSet)
-          [EC2ResponseObject.get_hash_from_response(rs.instancesSet.item)]
-        else
-          rs.collect {|r| 
-            if r.instancesSet.item.class == Array
-              r.instancesSet.item.map {|t| EC2ResponseObject.get_hash_from_response(t)}
-            else
-              [EC2ResponseObject.get_hash_from_response(r.instancesSet.item)]
-            end            
-          }.flatten.reject {|a| a.nil? }
-        end
-      rescue Exception => e
-        # Really weird bug with amazon's ec2 gem
-        rs.collect {|r| EC2ResponseObject.get_hash_from_response(r)}.reject {|a| a.nil? } rescue []
-      end
-
-      out
-    end
-    def self.get_response_from(resp)
-      begin
-        rs = resp.reservationSet.item unless resp.reservationSet.nil?
-        rs ||= resp.DescribeInstancesResponse.reservationSet.item
-        rs ||= rs.respond_to?(:instancesSet) ? rs.instancesSet : rs
-        rs.reject! {|a| a.nil? || a.empty? }
-      rescue Exception => e
-        resp
-      end
-      rs
-    end
-    def self.get_hash_from_response(resp)      
-      begin
-        {
-          :instance_id => resp.instanceId,
-          :name => resp.instanceId, 
-          :ip => resp.dnsName || "not-assigned",
-          :status => resp.instanceState.name,
-          :launching_time => resp.launchTime.parse_datetime,
-          :internal_ip => resp.privateDnsName,
-          :keypair => resp.keyName 
-          # :security_group => resp.groupSet.item[0].groupId
-        }        
-      rescue Exception => e
-        nil
-      end      
-    end
   end
 rescue LoadError
   puts <<-EOM
