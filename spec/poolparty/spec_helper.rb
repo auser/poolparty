c21a99418c9c9d0f63261543357ef33bb427ac76
Ari Lerner <ari.lerner@citrusbyte.com>
2008-12-11 20:02:41 -0800

    Added provisioner specs Updated cap spec Removed antiquated files

--- spec/poolparty/spec_helper.rb
+++ spec/poolparty/spec_helper.rb
@@ -105,7 +105,20 @@ def stub_list_from_remote_for(o, launch_stub=true)
   o.stub!(:launch_new_instance!).and_return sample_instances.first if launch_stub  
   stub_list_of_instances_for(o)
 end
-
+def stub_remoting_methods_for(o)
+  o.stub!(:keypair).and_return "fake_keypair"
+  o.stub!(:keypair_path).and_return "~/.ec2/fake_keypair"
+  o.stub!(:other_clouds).and_return []
+  o.stub!(:expand_when).and_return "cpu > 10"
+  o.stub!(:copy_file_to_storage_directory).and_return true
+  o.stub!(:rsync_storage_files_to).and_return true
+  o.stub!(:minimum_runnable_options).and_return []
+  o.stub!(:build_and_store_new_config_file).and_return true
+  o.stub!(:process_clean_reconfigure_for!).and_return true
+  o.stub!(:before_install).and_return true
+  o.stub!(:process_install).and_return true
+  o.stub!(:after_install).and_return true
+end
 def stub_list_of_instances_for(o)  
   # o.stub!(:list_of_running_instances).once.and_return running_remote_instances
   o.stub!(:keypair).and_return "fake_keypair"
