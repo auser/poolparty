2ad128a41d745ffedbb192b542380ab193afbb89
Ari Lerner <ari.lerner@citrusbyte.com>
2008-12-18 13:29:18 -0800

    Removed recursive directory listing and added to one level of depth searching for the clouds.rb file and updated gem to correspond

--- lib/poolparty/capistrano.rb
+++ lib/poolparty/capistrano.rb
@@ -39,11 +39,7 @@ module PoolParty
     
     def set_role_for_all_instances(role, name)
       ips = get_cloud(name).list_of_running_instances.map {|ri| ri.ip }
-      instance_eval returning Array.new do |arr|
-        ips.each do |ip|
-          arr << "role :#{role}, '#{ip}'"
-        end
-      end
+      ips.each {|ip| send :role, role.to_sym, "#{ip}"}
     end
   end
 end
