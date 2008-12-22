702a22c188b8698e223857696a625ef220ee1ffe
Ari Lerner <ari.lerner@citrusbyte.com>
2008-12-21 18:23:23 -0800

    Updated spec and master provisioning as well as gem for github

--- lib/poolparty/spec/core/string.rb
+++ lib/poolparty/spec/core/string.rb
@@ -1,11 +1,13 @@
 class String
   def grab_entry_for(type, name)
-    begin
-      allowed = /[ \$\._\-\[\]\n\t\\\/&,\(\)"',|:=\>\<0-9a-zA-Z]/
-      matches = self.match(/#{type}(.*)\{(.*)"#{name}":(.*)(#{allowed})*\}/)[0]
+    begin      
+      matches = self.match(/#{type}(.*)\{(.*)"#{name}":(.*)(#{_allowed})*\}/)[0]
     rescue
       ""
     end
+  end
+  def _allowed
+    /[ \$\._\*\-\[\]\n\t\\\/&,\(\)"',|:=\>\<A-Za-z0-9]/
   end
   def _grab_key_value_for(type, name, key)
     grab_entry_for(type, name).scan(/#{key}[ =>]*(.*)[,?]?$/).flatten.first rescue ""
