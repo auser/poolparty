eabfb306a8514a86d79c5a10e0d1d541180a4616
auser <arilerner@mac.com>
2009-01-20 14:25:12 -0800

    Updated version and history

--- lib/poolparty/version.rb
+++ lib/poolparty/version.rb
@@ -1,8 +1,8 @@
 module PoolParty
   module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 2
-    TINY  = 94
+    MAJOR = 1
+    MINOR = 0
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')    
   end
