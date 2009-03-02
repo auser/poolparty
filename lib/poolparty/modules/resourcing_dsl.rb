cbb15fe42721f3d9b7addf7aa6fa33e24808f1b8
Ari Lerner <alerner@ypcmc05591.yellowpages.local>
2009-03-02 11:51:53 -0800

    Added client-template exists

--- lib/poolparty/modules/resourcing_dsl.rb
+++ lib/poolparty/modules/resourcing_dsl.rb
@@ -8,7 +8,7 @@ module PoolParty
     def requires(str=nil)
       # str ? options.append!(:require => str) : options[:require]
       str ? options.append!(:require => send_if_method(str)) : options[:require]
-    end 
+    end
     def ensures(str="running")
       # if %w(absent running).map {|a| self.send a.to_sym}.include?(str)
         str == "absent" ? is_absent : is_present
@@ -49,14 +49,16 @@ module PoolParty
     end
     # Give us a template to work with on the resource
     # Make sure this template is moved to the tmp directory as well
-    # 
+    #
     # TODO: Change this method to store the template files for later
     # copying to prevent unnecessary copying and tons of directories
     # everywhere
     def template(file, opts={})
-      file = ::File.basename(file)
-      raise TemplateNotFound.new("no template given") unless file
-      
+      # require 'rubygems'; require 'ruby-debug'; debugger
+      puts file
+      filename = ::File.basename(file)
+      raise TemplateNotFound.new("no template given") unless filename
+      #TODO: check more MF
       unless opts[:just_copy]
         options.merge!({:content => "template(\"#{::File.basename(file)}\")"})
         options.delete(:source) if options.has_key?(:source)
@@ -66,19 +68,24 @@ module PoolParty
       end
     end
     
-    def get_client_or_gem_template(file)      
-      if client_templates_directory_exists? && client_template_exists?(file)
+    def get_client_or_gem_template(file)
+      if ::File.file?(file) && ::File.readable?(file)
+        file
+      elsif client_templates_directory_exists? && client_template_exists?(file)
         vputs "using custom template #{::File.join(Dir.pwd, "templates/#{file}")}"
         ::File.join(Dir.pwd, "templates/#{file}")
       else
         vputs "using standard template: #{::File.join(::File.dirname(__FILE__), "..", "templates/#{file}")}"
         ::File.join(::File.dirname(__FILE__), "..", "templates/#{file}")
-      end      
+      end
     end
+    
     def client_templates_directory_exists?
       ::File.directory?("#{Dir.pwd}/templates")
     end
+    
     def client_template_exists?(filename)
+      return true if ::File.file?(filename) && ::File.readable?(filename)
       file = ::File.join("#{Dir.pwd}/templates", filename)
       ::File.file?(file) && ::File.readable?(file)
     end
