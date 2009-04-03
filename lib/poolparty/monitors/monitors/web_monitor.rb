bef2c8b90a72b2175b5e9feb39f099bcc78c5486
Ari Lerner <arilerner@mac.com>
2009-04-03 03:01:45 -0700

    Checkpoint

--- lib/poolparty/monitors/monitors/web_monitor.rb
+++ lib/poolparty/monitors/monitors/web_monitor.rb
@@ -1,18 +1,18 @@
-=begin rdoc
-  Web monitor.
-=end
-module PoolParty
-  module Monitors
-    
-    class WebMonitor < BaseMonitor
-      
-      def run
-        str = %x[httperf]
-        str.split(/\s+/)[-1].to_f rescue 0.0
-      end
-            
-    end
-    
-    register_monitor :cpu
-  end
-end
+# =begin rdoc
+#   Web monitor.
+# =end
+# module PoolParty
+#   module Monitors
+#     
+#     class WebMonitor < BaseMonitor
+#       
+#       def run
+#         str = %x[httperf]
+#         str.split(/\s+/)[-1].to_f rescue 0.0
+#       end
+#             
+#     end
+#     
+#     register_monitor :cpu
+#   end
+# end
