58be6cab0e2f0a4535d975ee24c2650ecca7efac
fairchild <fairchild@stimble.net>
2009-04-02 22:08:52 -0700

    Refactor OrderedHash and add to_json to it. Reformat stderr output for netssh commands.

--- lib/poolparty/core/ordered_hash.rb
+++ lib/poolparty/core/ordered_hash.rb
@@ -1,50 +1,90 @@
 # Credit to Brian Candler on a ruby-forum post for this
 # http://www.ruby-forum.com/topic/166075
+require 'rubygems'
+require 'json'
 
-class OrderedHash < Hash
-  def initialize
-    @h, @a = {}, []
-    super
-  end
-  def [](k)
-    @h[k]
-  end
-  def []=(k,v)
-    @a << k unless @h.has_key?(k)
-    @h[k] = v
-  end
-  def merge(o={})
-    na = dup
-    o.each {|k,v| na[k] = v }
-    na
-  end
-  def keys
-    map {|k,v| k }
-  end
-  def size
-    @h.size
-  end
-  def merge!(o={})
-    o.each {|k,v| self[k] = v }
-  end
-  def each
-    @a.each { |k| yield k,@h[k] }
-  end
+class OrderedHash
+	include Enumerable
+	def initialize
+		@h = {}
+		@a = []
+	end
+	def [](k)
+		@h[k]
+	end
+	def []=(k,v)
+		@a << k unless @h.has_key?(k)
+		@h[k] = v
+	end
+	def merge(o={})
+		na = dup
+		o.each {|k,v| na[k] = v }
+		na
+	end
+	def keys
+		map {|k,v| k }
+	end
+	def has_key?(k)
+	 
+	end
+	def values
+		map {|k,v| v }
+	end
+	def size
+		@h.size
+	end
+	def merge!(o={})
+		o.each {|k,v| self[k] = v }
+	end
+	def each
+		@a.each { |k| yield k,@h[k] }
+	end
+	
+	#TODO: Need specs below here
+	def each_value
+		@a.each {|k| yield(@h[k])}
+	end
+	def each_key
+		@a.each {|k| yield k}
+	end
+	def clear
+		@a.clear
+		@h.clear
+	end
+	def delete(k, &block)
+		@a.delete k
+		@h.delete(k, &block)
+	end
+	def reject!
+		del = []
+		each_pair {|k,v| del << k if yield k,v}
+		del.each {|k| delete k}
+		del.empty? ? nil : self
+	end
+	def delete_if(&block)
+		reject!(&block)
+		self
+	end
+	def method_missing(*args)
+	  @h.send(*args)
+	end
+	def to_hash
+		self
+	end
+	def to_h
+		self
+	end
+	def to_json
+	  "{#{collect{|k, v| "\"#{k}\":#{v.to_json}"}.join(',')}}"
+	end
+	
 end
 
-
+# 
 # oh = OrderedHash.new
-# oh["var1"]=123
-# oh["var2"]=2
-# oh["var3"]=3
-# oh["var4"]=23
+# oh["var1"]=3
+# oh["var2"]=Time.now
+# oh["var3"]=[1,2,3]
+# oh["var4"]={:keyme=>'valueme'}
 # oh.each { |k,v| puts "#{k}=>#{v}" }
-# puts oh["var2"]
-# puts oh.collect { |k,v| k }.inspect  #=> ["var1", "var2", "var3", "var4"]
-
-# result =>
-# var1=>123
-# var2=>2
-# var3=>3
-# var4=>23
-# 2
+# p oh.to_json
