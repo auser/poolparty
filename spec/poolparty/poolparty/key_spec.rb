79906eb4c1df2a52ab9529fc168a23bfc76aedfc
Ari Lerner <arilerner@mac.com>
2009-04-03 13:34:57 -0700

    Merged fairchild/mastermerge

--- spec/poolparty/poolparty/key_spec.rb
+++ spec/poolparty/poolparty/key_spec.rb
@@ -7,6 +7,8 @@ describe "Key" do
   it "should fill in the filepath as id_rsa if no filelocation is given" do
     Key.new.filepath.should == "id_rsa"
   end
+  it "should provide valid to_json" do
+    lambda {Key.new("file").to_json}.should_not raise_error  end
   describe "that exists" do
     before(:each) do
       @keypair = "/var/home/id_rsa"
