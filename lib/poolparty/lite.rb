eb2e544d9f9aa8f240dbfc372a1942533cd1322c
Ari Lerner <arilerner@mac.com>
2009-04-07 15:48:06 -0700

    Added constantize to string

--- lib/poolparty/lite.rb
+++ lib/poolparty/lite.rb
@@ -10,6 +10,16 @@ module PoolParty
     json_file = json_file_path || PoolParty::Default.properties_hash_file
     PoolParty::Schema.new( ::File.read(json_file) )
   end
+  
+  def self.constantize(name, base_object=PoolParty)
+    begin
+      const = base_object.constants.find{|cnst| cnst == name.camel_case}
+      base_object.module_eval const
+    rescue Exception => e
+      puts "#{name.camel_case} is not defined. #{e}"
+      nil
+    end
+  end
 end
 
 class String
