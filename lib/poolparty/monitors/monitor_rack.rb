426d6ce0af1f1d3008c3d0ec78e70805a716f510
fairchild <fairchild@stimble.net>
2009-04-09 17:03:17 -0700

    Add a basic rack app that will map a method to a class and method. Monitors inherit from MonitorRack so GET /stats/load would map to Monitors::Stats.send(:load)

---
+++ lib/poolparty/monitors/monitor_rack.rb
@@ -0,0 +1,118 @@
+require ::File.dirname(__FILE__)+"/../aska/aska.rb"
+require ::File.dirname(__FILE__)+"/../lite.rb"
+
+require 'rubygems'
+require 'rack'
+require 'json'
+
+class String
+  def camelcase
+    gsub(/(^|_|-)(.)/) { $2.upcase }
+  end
+end
+
+module Monitors
+  
+  # def self.call(env)
+  #   @request = Rack::Request.new(env)
+  #   @response = Rack::Response.new
+  #   begin
+  #     response.write map_to_class_method(path_map(env)).to_json
+  #   rescue Exception=>e
+  #     @response.write e
+  #     @response.status = 500
+  #   end
+  #   @response.finish # [response.status, response.headers, response.body]
+  # end
+
+  class MonitorRack
+    
+    def new(opts={})
+      @opts=opts
+    end
+    
+    def call(env)
+      @env = env
+      @request = Rack::Request.new env
+      @response = Rack::Response.new
+      
+      begin
+        path_array= path_map(env) || []
+        puts "-- trying to map #{path_array.inspect}"
+        @response.write map_to_class_method(path_array).to_json
+      # rescue Exception=>e
+      #   @response.write e
+      #   @response.status = 500
+      end
+      @response.finish # [response.status, response.headers, response.body]
+    end
+    
+    def request
+      @request
+    end
+    def response
+      @response
+    end
+  
+    def path_map(env)
+      env['REQUEST_PATH'].split('.')[0].split('/')[1..-1]
+    end
+  
+    # Find class and call method from the pattern /class_name/method/args
+    def map_to_class_method(path)
+      if !path or path.empty? or path[0].nil?
+        response.write 'cannot map an empty path' 
+        response.status='404'
+      else
+        klass = constantize(path[0])
+        raise "#{path[0]} did not map to a Constant" if !klass
+        case path.size
+        when 0
+          default
+        when 1
+          klass.send(:default) rescue klass.new.send(:default)
+        when 2
+          klass.send(path[1].to_sym) rescue klass.new.send(path[1].to_sym)
+        else
+          klass.send(path[1].to_sym, *path[2..-1])
+        end
+      end
+    end
+    
+    def constantize(name, base_object=Monitors)
+      begin
+        const = base_object.constants.find{|cnst| cnst == name.camelcase}
+        base_object.module_eval const
+      rescue Exception => e
+        puts "#{name.camelcase} is not defined. #{e}"
+        nil
+      end
+    end
+  
+    def default
+      'default response'
+    end
+  
+  end
+  
+  #load our monitors
+  require ::File.dirname(__FILE__)+"/stats_monitor.rb"
+  
+
+  # just here as an example
+  # access it at /monitor_time/ or /monitor_time/now
+  class MonitorTime
+    def self.default
+      now
+    end
+    def self.now
+      Time.now
+    end
+  end
+  
+  class Favicon
+    def self.default
+      ''
+    end
+  end
+end
---
+++ lib/poolparty/monitors/stats_monitor.rb
@@ -0,0 +1,184 @@
+require ::File.dirname(__FILE__)+"/monitor_rack.rb"
+
+module Monitors
+    
+    class Stats 
+      attr_reader :stats
+
+      def initialize(o={})
+        @cloud = JSON.parse( open(o[:clouds_json_file]).read ) rescue {"options" => 
+                                                                        {"rules" => {"expand" => PoolParty::Default.expand_when, 
+                                                                                      "contract" => PoolParty::Default.contract_when
+                                                                                    }}}
+        # Our cloud.options.rules looks like
+        #  {"expand_when" => "load > 0.9", "contract_when" => "load < 0.4"}
+        # We set these as rules on ourselves so we can use aska to parse the rules
+        # So later, we can call vote_rules on ourself and we'll get back Aska::Rule(s)
+        # which we'll call valid_rule? for each Rule and return the result
+        @cloud["options"]["rules"].each do |name, rul|
+          r = Aska::Rule.new(rul)
+          rule(name) << r
+        end
+        # fork_and_put
+        log << "#{Time.now.strftime("%Y-%m-%d-%H-%M")}, #{stats.to_json}\n"
+      end
+      
+      def default
+        stats
+      end
+
+      def log(log_file_path="/var/log/poolparty/stats_monitor.log")
+        ::File.file? log_file_path
+        @logfile ||= ::File.new(log_file_path, 'a')
+      end
+
+      def get(req=request, resp=response)
+        begin
+          if !req.params || req.params.empty?
+            default_stats
+          else
+            stats[req.params[0].to_sym] ||= self.send(req.params[0])
+            stats[req.params[0].to_sym]
+          end
+        rescue Exception => e
+          resp.fail!
+          "Error: #{e}"
+        end
+      end
+
+      def put(req=request, resp=response)
+        if d = JSON.parse(req.post_content)
+          hsh = d.reject {|ip, _node| ip == my_ip }
+          stats.merge!(hsh)
+          handle_election
+        else
+          "boom"
+        end
+      end
+
+      # Handle the elections
+      def handle_election
+        # Ballots look like:
+        # host => ["contract"]
+        candidates = {:expand => 0, :contract => 0}
+        candidates.each do |action, ballots|
+          stats.each do |ip, node_hsh|
+            candidates[action]+=1 if node_hsh["nominations"] && node_hsh["nominations"].include?(action.to_s)
+          end
+        end
+        # TODO: Move?
+        # Expand the cloud if 50+% of the votes are for expansion
+        # Contract the cloud if 51+% of the votes are for contraction
+        if (candidates[:expand] - candidates[:contract])/stats.keys.size > 0.5
+          %x[/usr/bin/server-cloud-elections expand] unless elected_action == "expand"
+          @elected_action = "expand"
+        elsif (candidates[:contract] - candidates[:expand])/stats.keys.size > 0.5
+          %x[/usr/bin/server-cloud-elections contract] unless elected_action == "contract"
+          @elected_action = "contract"
+        end      
+
+        reload_data!
+        stats[my_ip]["elected_action"] = @elected_action if @elected_action
+        log << "#{Time.now.strftime("%Y-%m-%d-%H-%M")}, #{stats.to_json}\n"
+        fork_and_put
+        "ok"
+      end
+
+      def fork_and_put
+        fork do
+          # put to next node
+          # TODO: Fix mysterious return of the nil (HASH next_sorted_key(my_ip))
+          # next_node = stats.next_sorted_key(my_ip)        
+          idx = (stats.size - stats.keys.sort.index(my_ip))
+          next_node = stats.keys.sort[idx - 1]
+          sleep(10)
+          Net::HTTP.start(next_node, PoolParty::Default.butterfly_port) do |http|
+            http.send_request('PUT', '/stats_monitor.json', stats.to_json)
+          end
+        end
+      end
+
+      def elected_action
+        @elected_action ||= nil
+      end
+
+      def rules
+        @rules ||= {}
+      end
+
+      def rule(name)
+        rules[name] ||= []
+      end
+
+      def default_stats
+        %w(load nominations).each do |var|
+          stats[my_ip][var] ||= self.send(var.to_sym)
+        end
+        stats
+      end
+
+      def stats
+        @stats ||= {my_ip  => {}}
+      end
+
+      def load
+        %x{"uptime"}.split[-3].to_f
+      end
+
+      def instances
+        # res = PoolParty::Neighborhoods.load_default.instances
+        res ||= %x[/usr/bin/server-list-active internal_ip].split("\t")
+        res
+      end
+
+      def can_expand?
+        instances.size < max_instances
+      end
+
+      def can_contract?
+        instances.size > min_instances
+      end
+
+      def min_instances
+        (@cloud["options"]["minimum_instances"] || PoolParty::Default.minimum_instances).to_i
+      end
+
+      def max_instances
+        (@cloud["options"]["maximum_instances"] || PoolParty::Default.maximum_instances).to_i
+      end
+
+      def nominations
+        load = stats[my_ip]["load"] ||= self.send(:load)
+        stats[my_ip]["nominations"] ||= rules.collect do |k,cld_rules|
+          t = cld_rules.collect do |r|
+            # If the comparison works
+            if self.send(r.key.to_sym).to_f.send(r.comparison, r.var.to_f)
+              # if we are facing an expansion rule
+              if k =~ /expand/
+                k if can_expand?
+              # if we are facing a contraction rule
+              elsif k =~ /contract/
+                k if can_contract?
+              else
+                k
+              end
+            end
+          end.compact
+        end.flatten.compact
+      end
+
+      def my_ip
+        @my_ip ||= ohai["ipaddress"]
+      end
+
+      def ohai
+        @ohai ||= JSON.parse(%x[ohai])
+      end
+
+      def reload_data!
+        @stats[my_ip] = {}
+        instances.each {|inst| @stats[inst] = {} }
+      end    
+    
+  end
+end
