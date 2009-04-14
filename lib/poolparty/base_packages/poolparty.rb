f84b6e6f8a9a3f7e6ef78eedcda4ddbc9a989015
Ari Lerner <arilerner@mac.com>
2009-04-14 16:31:18 -0700

    Added git as a default package

--- lib/poolparty/base_packages/poolparty.rb
+++ lib/poolparty/base_packages/poolparty.rb
@@ -3,6 +3,8 @@ module PoolParty
     plugin :poolparty_base_packages do
       
       def enable
+        has_package "git"
+        
         has_cron "/usr/bin/server-manage-election" do
           minute "0,5,10,15,20,25,30,35,40,45,50,55"
           command "/usr/bin/server-manage-election"
