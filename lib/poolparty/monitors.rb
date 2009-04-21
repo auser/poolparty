011846dbd035b51a3b6e060f5436dd53c16df81f
fairchild <fairchild@stimble.net>
2009-04-20 21:35:27 -0700

    Cleaned up monitors/ MonitorRack will now map http methods, for example   GET /neighborhood/size => Monitors::Neighboorhood.get_size

--- lib/poolparty/monitors.rb
+++
@@ -1,6 +0,0 @@
-require "rubygems"
-require "json"
-require ::File.join(::File.dirname(__FILE__), 'monitors/', 'monitor_rack.rb') 
-
-module PoolParty
-end
--- lib/poolparty/monitors/monitor_rack.rb
+++ lib/poolparty/monitors/monitor_rack.rb
@@ -1,9 +1,15 @@
+=begin rdoc
+  MonitorRack is a rack application that maps url requests to method calls on Monitor classes.
+=end
+
 require ::File.dirname(__FILE__)+"/../aska/aska.rb"
 require ::File.dirname(__FILE__)+"/../lite.rb"
 
 require 'rubygems'
 require 'rack'
 require 'json'
+
+Dir[::File.dirname(__FILE__)+"/monitors/*"].each {|m| puts "require #{m}"; require "#{m}"}
 
 class String
   def camelcase
@@ -12,37 +18,18 @@ class String
 end
 
 module Monitors
-  
-  # def self.call(env)
-  #   @request = Rack::Request.new(env)
-  #   @response = Rack::Response.new
-  #   begin
-  #     response.write map_to_class_method(path_map(env)).to_json
-  #   rescue Exception=>e
-  #     @response.write e
-  #     @response.status = 500
-  #   end
-  #   @response.finish # [response.status, response.headers, response.body]
-  # end
 
   class MonitorRack
     
-    def new(opts={})
-      @opts=opts
-    end
-    
     def call(env)
-      dup._call(env)
-    end
-    
-    def _call(env)
       @env = env
       @request = Rack::Request.new env
       @response = Rack::Response.new
       begin
         path_array= path_map(env['REQUEST_PATH']) || []
-        puts "-- trying to map #{path_array.inspect}"
-        @response.write map_to_class_method(path_array).to_json
+        verb = env['REQUEST_METHOD'].downcase
+        # puts "-- trying to map #{path_array.inspect}"
+        @response.write map_to_class_method(path_array, verb).to_json
       # rescue Exception=>e
       #   @response.write e
       #   @response.status = 500
@@ -60,12 +47,16 @@ module Monitors
       @request
     end
     
+    # Split the request path into an array
     def path_map(requested_path)
       requested_path.split('.')[0].split('/')[1..-1]
     end
   
     # Find class and call method from the pattern /class_name/method/args
-    def map_to_class_method(path)
+    # GET /neighborhood => ::Monitors::Neighboorhood.get
+    # POST /neighborhood => ::Monitors::Neighboorhood.post(params)
+    # GET /neighborhood/size => ::Monitors::Neighboorhood.get_size
+    def map_to_class_method(path, verb='get')
       if !path or path.empty? or path[0].nil?
         response.write 'cannot map an empty path' 
         response.status='404'
@@ -74,17 +65,18 @@ module Monitors
         raise "#{path[0]} did not map to a Constant" if !klass
         case path.size
         when 0
-          self.respond_to?(:default) ? self.send(:default) : response.status='404'
-        when 1
-          klass.send(:default) rescue klass.new(env).send(:default)
-        when 2
-          klass.send(path[1].to_sym) rescue klass.new(env).send(path[1].to_sym)
+          self.respond_to?(verb.to_sym) ? self.send(verb.to_sym) : response.status='404'
+        when 1 #/stats
+          klass.send(verb.to_sym) rescue klass.new(env).send(verb.to_sym)
+        when 2 #/stats/load
+          klass.send("#{verb}_#{path[1]}".to_sym) #rescue klass.new(env).send("#{verb}_#{path[1]}".to_sym)
         else
-          klass.send(path[1].to_sym, *path[2..-1])
+          klass.send("#{verb}_#{path[1]}".to_sym, *path[2..-1])
         end
       end
     end
     
+    # Take a string and return a ruby object if a match is found in the base_objects namespace.
     def constantize(name, base_object=Monitors)
       begin
         const = base_object.constants.find{|cnst| cnst == name.camelcase}
@@ -95,38 +87,6 @@ module Monitors
       end
     end
   
-    def default
-      'default response'
-    end
-  
   end
-  
-  # Base monitor
-  class MonitorBase
-    def initialize(env={})
-      @env = env
-    end
-  end
-  
-  #load our monitors
-  require ::File.dirname(__FILE__)+"/stats_monitor.rb"
-  require ::File.dirname(__FILE__)+"/neighborhood_monitor.rb"
-  
 
-  # just here as an example
-  # access it at /monitor_time/ or /monitor_time/now
-  class MonitorTime
-    def self.default
-      now
-    end
-    def self.now
-      Time.now
-    end
-  end
-  
-  class Favicon < MonitorBase
-    def self.default
-      ''
-    end
-  end
 end
