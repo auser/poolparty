17f92e05db7fb17a95d6127d40569b1996c9f5af
Ari Lerner <arilerner@mac.com>
2009-04-29 02:23:23 -0700

    Added are you sure msg on cloud terminate

--- lib/poolpartycl.rb
+++ lib/poolpartycl.rb
@@ -5,3 +5,17 @@ end
 def help_array
   ["-h", "--help", "-V", "--version", "--debug", "-d"]
 end
+
+def are_you_sure?(msg)
+  puts msg
+  resp = gets.strip!
+
+  case resp
+  when "Y"
+  when "yes"
+  when "y"
+    return true
+  else
+    return false
+  end
+end
