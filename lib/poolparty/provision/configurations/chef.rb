1612f768dc8fd8df43581225defb06372a417dc9
Ari Lerner <arilerner@mac.com>
2009-04-29 19:56:52 -0700

    Added debugging flag to chef (thanks pingali)

--- lib/poolparty/provision/configurations/chef.rb
+++ lib/poolparty/provision/configurations/chef.rb
@@ -3,12 +3,13 @@ module PoolParty
     
     class Chef
       def self.commands
+        debug_flag = "-l debug" if debugging? 
         [
           "mkdir -p /etc/chef/cookbooks /etc/chef/cache",
           "cp -R /var/poolparty/dr_configure/chef/cookbooks/* /etc/chef/cookbooks",
           "cp /var/poolparty/dr_configure/chef/solo.rb /etc/chef/solo.rb",
           "cp /var/poolparty/dr_configure/chef/dna.json /etc/chef/dna.json",
-          "/usr/bin/chef-solo -c /etc/chef/solo.rb -j /etc/chef/dna.json"
+          "/usr/bin/chef-solo #{debug_flag} -c /etc/chef/solo.rb -j /etc/chef/dna.json"
         ]
       end
       def self.files_to_upload
