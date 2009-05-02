1c2a04ff6b8c21341654f96b60cafe7a22ec8fe0
Nate Murray <nate@natemurray.com>
2009-05-01 17:18:05 -0700

    added http_match and http_status verifiers

---
+++ lib/poolparty/verification/verifiers/http_status.rb
@@ -0,0 +1,59 @@
+module PoolParty
+  module Verifiers
+    
+=begin
+
+== HttpStatus Verifier
+
+Verify the HTTP response code from a particular url
+
+== Usage 
+
+  http_status url, response_code
+
+== Example
+
+  verify do
+    http_status "http://host/index.html", 200
+    http_status "http://host/index.html", :success
+    http_status "http://host/asdfasdfads.html", 404
+  end
+
+=end
+
+    class HttpStatus < VerifierBase
+      require 'net/http'
+      require 'uri'
+      
+      attr_reader :uri, :status
+      def initialize(uri, status)
+        @uri    = URI.parse(uri)
+        @status = status
+      end
+
+      def passing?
+        http = Net::HTTP.new(@uri.host)
+        response = http.request_get(@uri.path)
+
+        return case true
+               when status.kind_of?(Numeric)
+                 response.code == @status.to_s 
+               when status.kind_of?(Symbol)
+                 case status
+                 when :success then response.kind_of?(Net::HTTPSuccess)
+                 else 
+                   raise "unknown symbol #{status}"
+                 end
+               else
+                 raise "unknown status type #{status}"
+               end
+      end
+
+      def to_s
+        "<#{self.class.to_s} uri:#{uri} status:#{status}>"
+      end
+
+    end
+    
+  end
+end
