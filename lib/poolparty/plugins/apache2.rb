b43e4aaedd83d4472dc1b48d2e5107251b090f7a
Nate Murray <nate@natemurray.com>
2009-05-04 11:32:43 -0700

    started adding in old apache2 plugin

--- lib/poolparty/plugins/apache2.rb
+++ lib/poolparty/plugins/apache2.rb
@@ -1,51 +1,316 @@
 module PoolParty
   class Base
+
     plugin :apache do
+      def enable
+        install
+      end
+            
+      def install
+        installed_as_worker
+      end
       
-      # Called after the plugin is loaded entirely, all the options are set, etc.
-      def loaded(o={}, &block)
-        include_chef_recipe chef_apache2_recipe_root # We want to include the chef apache2 recipe to use it
+      def install_passenger
+        base_install
+        enable_passenger
+      end
+      
+      def base_install        
+        unless @base_install
+          has_exec({:name => "restart-apache2", :command => "/etc/init.d/apache2 restart", :refreshonly => "true"})
+          has_exec({:name => "reload-apache2", :command => "/etc/init.d/apache2 reload", :refreshonly => "true"})
+          has_exec({:name => "force-reload-apache2", :command => "/etc/init.d/apache2 force-reload", :refreshonly => "true"})
+
+          vars
+          configs
+          @base_install = true
+        end
+      end
+
+      def installed_as_worker            
+        unless @installed_as_worker
+          has_package(:name => "apache2")
+          has_package(:name => "apache2-mpm-worker", :ensures => "installed")
+          has_service(:name => "apache2") do
+            requires get_package("apache2")
+            ensures "running"
+          end
+
+          # Should clean this up
+          does_not_have_package(:name => "apache2-mpm-event")
+          does_not_have_package(:name => "apache2-mpm-perchild")
+          does_not_have_package(:name => "apache2-mpm-prefork")
+
+          base_install
+          @installed_as_worker = true
+        end            
+      end
+
+      # include chef passenger
+      
+      def enable_passenger
+        unless @enable_passenger
+          installed_as_worker
+          has_package(:name => "build-essential")
+          has_package(:name => "apache2-prefork-dev")
+          has_gempackage(:name => "fastthread")
+
+          has_exec(:name => "install_passenger_script", 
+            :command => "/usr/bin/ruby #{Base.remote_storage_path}/install_passenger.rb", 
+            :ifnot => "test -f /etc/apache2/conf.d/passenger.conf && test -s /etc/apache2/conf.d/passenger.conf",
+            :notify => get_service("apache2")
+            )
+
+          has_file(:name => "#{Base.remote_storage_path}/install_passenger.rb", :notify => get_exec("install_passenger_script")) do
+            template File.dirname(__FILE__)/".."/"templates"/"apache2"/"install_passenger.rb"
+            # template "install_passenger.rb" # todo
+          end
+
+          has_gempackage(:name => "passenger", :notify => get_exec("install_passenger_script"))
+          
+          @enable_passenger = true
+        end
+      end
+      
+      def enable_ssl
+        unless @enable_ssl
+          has_package("apache2.2-common")
+          has_package("openssl")
+          has_variable(:name => "ssl_enabled", :value => "true")
+
+          has_exec(:command => "a2enmod ssl") do
+            requires [ get_package("openssl") ]
+            ifnot "/usr/bin/test -L /etc/apache2/mods-enabled/ssl.load"
+            notify get_exec("restart-apache2")
+          end
+          @enable_ssl = true
+        end
+      end
+      
+      def configs
+        unless @configs
+          has_directory(:name => "/etc/apache2") do
+            has_directory(:name => "/etc/apache2/conf.d")
+            has_directory(:name => "/etc/apache2/site-includes")
+          end          
+
+          has_file(:name => "/etc/apache2/apache2.conf") do
+            mode 644
+            requires get_directory("/etc/apache2/conf.d")
+            template File.dirname(__FILE__)/"templates"/"apache2"/"apache2.conf"
+          end
+          does_not_have_file(:name => "/etc/apache2/ports.conf")
+          has_exec(:command => "/usr/sbin/a2dissite default") do
+            onlyif "/usr/bin/test -L /etc/apache2/sites-enabled/000-default"
+            notify get_exec("reload-apache2")
+          end
+
+          # Base config
+          config("base", ::File.join(File.dirname(__FILE__), "templates/apache2", "base.conf.erb"))
+          config("mime", ::File.join(File.dirname(__FILE__), "templates/apache2/", "mime-minimal.conf.erb"))
+          config("browser_fixes", ::File.join(File.dirname(__FILE__), "templates/apache2", "browser_fixes.conf.erb"))
+
+          present_apache_module("mime", "rewrite")
+        # end
+          @configs = true
+        end
+      end
+      
+      def config(name, temp)
+        has_file(:name => "/etc/apache2/conf.d/#{name}.conf") do
+          template temp
+          runs get_exec("reload-apache2")
+        end
+      end
+      
+      def listen(port="8080")
+        has_variable(:name => "port", :value => port)
+      end
+      
+      def site(name, opts={})
+        case opts[:ensure] || "present"
+        when "present", "installed"
+          install_site(name, opts)
+        when "absent"
+          has_exec(:command => "/usr/sbin/a2dissite #{name}", :notify => get_exec("reload-apache2")) do
+          requires get_package("apache2")
+            onlyif "/bin/sh -c \"[ -L /etc/apache2/sites-enabled/#{name} ] && [ /etc/apache2/sites-enabled/#{name} -ef /etc/apache2/sites-available/#{name}]\""
+          end
+        end
+      end
+      
+      def install_site(name, opts={})
+        options = opts.merge!(:name => "/etc/apache2/sites-available/#{name}", :ensures => 'present', :alias => "site-#{name}")
+        has_directory(:name => "/etc/apache2/sites-available")
+        has_file(options)
+        has_exec(:command => "/usr/sbin/a2ensite #{name}", :notify => get_exec("reload-apache2"), :requires => get_file("site-#{parent.name}")) do
+          requires get_package("apache2")
+          ifnot "/bin/sh -c \"[ -L /etc/apache2/sites-enabled/#{name} ] && [ /etc/apache2/sites-enabled/#{name} -ef /etc/apache2/sites-available/#{name} ]\""
+        end
+      end
+      
+      def site_include(name, content, ensureer="present")
+        has_file(:name => "/etc/apache2/site-includes/#{name}.inc", :ensures => ensureer, :content => content, :requires => get_file("/etc/apache2/site-includes"))
       end
       
       def present_apache_module(*names)
         names.each do |name|
-          has_chef_recipe "apache2::mod_" + name
+          exec(:name => "mod-#{name}", :command => "/usr/sbin/a2enmod #{name}") do
+            requires get_package("apache2")
+            ifnot "/bin/sh -c \"[ -L /etc/apache2/mods-enabled/#{name}.load ] \\
+              && [ /etc/apache2/mods-enabled/#{name}.load -ef /etc/apache2/mods-available/#{name}.load ]\""
+            notify get_exec("force-reload-apache2")            
+          end
         end
       end
-
-      private
-      def chef_apache2_recipe_root
-        "#{::File.dirname(__FILE__)}/../../../vendor/chef/apache2"
+      
+      def absent_apache_module(*names)
+        names.each do |name|
+          exec({:name => "no-mod-#{name}"}, :command => "/usr/sbin/a2dismod #{name}") do
+            requires get_package("apache2")
+            ifnot "/bin/sh -c \"[ -L /etc/apache2/mods-enabled/#{name}.load ] \\
+    					&& [ /etc/apache2/mods-enabled/#{name}.load -ef /etc/apache2/mods-available/#{name}.load ]\""
+    				notify get_exec("force-reload-apache2")
+          end
+        end
+      end
+      
+      # Templates
+      def virtual_host_template(name, opts={})
+        template ::File.join(File.dirname(__FILE__), "templates/webserver", "virtual_host.conf.erb")
       end
       
     end
 
-
-    # Usage:°
-    #°
-    # enable_php5 do
-    #   extras :cli, :pspell, :mysql
-    # end
-    virtual_resource(:enable_php5) do
-      def loaded(opts={}, parent=self)
-        has_package("php5")
-        has_package("libapache2-mod-php5")
-        present_apache_module("php5")
-        has_file({:name => "/etc/php5/apache2/php.ini",
-                :template => File.dirname(__FILE__) + "/../templates/php.ini.erb",
-                :mode => 755,
-                :requires => get_package("libapache2-mod-php5")})
-                # :notify => get_exec("reload-apache2")})
-      end
-
-      def extras(*names)
-        names.each do |name|
-          # has_package(:name => "php5-#{name}", :requires => get_package("php5"))
-          has_package(:name => "php5-#{name}")
-        end
-      end
-
+  virtual_resource(:virtualhost) do
+    def listen(port="8080")
+      has_variable(:name => "port", :value => port)
+      port port
     end
+        
+    def virtual_host_entry(file)
+      if ::File.file?(file)
+        has_file(options.merge({:name => "/etc/apache2/sites-available/#{name}", :ensures => 'present', :alias => "#{name}", :template => file, :requires => get_package("apache2")}))
+      else
+        has_file(options.merge({:content => file, :name => "/etc/apache2/sites-available/#{name}", :ensures => 'present', :alias => "#{name}", :requires => get_package("apache2")}))
+      end
+    end
+    
+    def loaded(opts={}, parent=self)
+      has_directory(:name => "/var/www")
+      has_directory(:name => "/var/www/#{name}")
+      has_directory(:name => "/var/www/#{name}/logs", :owner => "www-data")
+
+      has_variable(:name => "sitename", :value => "#{name}")
+      
+      has_exec(:name => "insert-site-#{name}", :command => "/usr/sbin/a2ensite #{name}", :notify => get_exec("reload-apache2"), :requires => get_file("/etc/apache2/sites-available/#{name}")) do
+        requires get_package("apache2")
+        ifnot "/bin/sh -c \"[ -L /etc/apache2/sites-enabled/#{parent.name} ] && [ /etc/apache2/sites-enabled/#{parent.name} -ef /etc/apache2/sites-available/#{parent.name} ]\""
+      end
+    end
+  end
+  
+  virtual_resource(:passengersite) do    
+    def loaded(opts={}, parent=self)
+      enable_passenger
+      passenger_entry <<-EOE
+Listen #{port ? port : "8080"}
+<VirtualHost *>
+    ServerName #{name}
+    DocumentRoot #{docroot || "/var/www/#{name}"}/public
+    RailsEnv production
+    ErrorLog #{docroot || "/var/www/#{name}"}/log/error_log
+    CustomLog #{docroot || "/var/www/#{name}"}/log/access_log common
+</VirtualHost>
+      EOE
+      
+      has_directory(:name => "/var/www")
+      has_directory(:name => "/var/www/#{name}")
+      has_directory(:name => "/var/www/#{name}/log", :requires => get_directory("/var/www/#{name}/"))
+      
+      has_variable(:name => "sitename", :value => "#{name}")      
+      
+      has_exec(:command => "/usr/sbin/a2ensite #{name}", :notify => 'Exec["reload-apache2"]', :requires => get_file("/etc/apache2/sites-available/#{name}")) do
+        ifnot "/bin/sh -c \"[ -L /etc/apache2/sites-enabled/#{name} ] && [ /etc/apache2/sites-enabled/#{name} -ef /etc/apache2/sites-available/#{name} ]\""
+      end
+    end
+    def passenger_entry(file)
+      if ::File.file?(file)
+        has_file({:name => "/etc/apache2/sites-available/#{name}", :ensures => 'present', :alias => "#{name}", :template => file, :requires => get_package("apache2")})
+      else
+        has_file({:content => file, :name => "/etc/apache2/sites-available/#{name}", :ensures => 'present', :alias => "#{name}", :content => file, :requires => get_package("apache2")})
+      end
+    end
+  end
+  
+  virtual_resource(:passengersite_with_ssl) do
+    def loaded(opts={}, parent=self)
+      enable_passenger
+
+      has_variable(:name => "virtualhost_name", :value => name)
+      has_variable(:name => "port", :value => port.nil? ? "8080" : port)
+            
+      has_directory(:name => "/var/www")
+      has_directory(:name => "/var/www/#{name}")
+      has_directory(:name => "/var/www/#{name}/log", :requires => get_directory("/var/www/#{name}/"))
+      
+      has_variable(:name => "passenger_name", :value => name)
+      
+      has_file(options.merge({:name => "/etc/apache2/sites-available/#{name}", :ensures => 'present', :alias => "#{name}", :requires => get_package("apache2")})) do
+        template File.dirname(__FILE__), "/../templates/webserver", "passenger.conf.erb"
+      end
+      
+      has_exec(:command => "/usr/sbin/a2ensite #{name}", :notify => 'Exec["reload-apache2"]', :requires => get_file("/etc/apache2/sites-available/#{name}")) do
+        ifnot "/bin/sh -c \"[ -L /etc/apache2/sites-enabled/#{@parent.name} ] && [ /etc/apache2/sites-enabled/#{@parent.name} -ef /etc/apache2/sites-available/#{@parent.name} ]\""
+      end
+    end
+    
+    # tell apache ssl which certificate/private key files to use for this virtual site
+    def certificate(cert_path, key_path)
+      cert_name = File.basename(cert_path)
+      key_name = File.basename(key_path)
+      has_variable(:name => "ssl_cert_file", :value => "/var/www/#{name}/cert/#{cert_name}")
+      has_variable(:name => "ssl_private_key_file", :value => "/var/www/#{name}/cert/#{key_name}")
+    
+      has_directory(:name => "/var/www")
+      has_directory(:name => "/var/www/#{name}")
+      has_directory(:name => "/var/www/#{name}/cert", :requires => get_directory("/var/www/#{name}/"))
+    
+      has_file(:name => "/var/www/#{name}/cert/#{cert_name}") do
+        content open(cert_path).read
+      end
+      has_file(:name => "/var/www/#{name}/cert/#{key_name}") do
+        content open(key_path).read
+      end
+    end    
+  end
+
+  # Usage: 
+  # 
+  # enable_php5 do
+  #   extras :cli, :pspell, :mysql
+  # end
+  virtual_resource(:enable_php5) do
+    def loaded(opts={}, parent=self)
+      has_package(:name => "php5")
+      has_package(:name => "libapache2-mod-php5")
+      present_apache_module("php5")
+        has_file({:name => "/etc/php5/apache2/php.ini",
+                :template => File.dirname(__FILE__) + "/../templates/apache2/php.ini.erb",
+                :mode => 755,
+                :requires => get_package("libapache2-mod-php5"),
+                :notify => get_exec("reload-apache2")})
+    end
+
+    def extras(*names)
+      names.each do |name|
+       has_package(:name => "php5-#{name}", :requires => get_package("php5"))
+      end
+    end
+
+  end
+  
+
 
 
   end
