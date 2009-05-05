b5952477a790d56e2512b21c7a1b6e99430aad2e
Nate Murray <nate@natemurray.com>
2009-05-05 09:52:11 -0700

    searchable paths now work. now need to roll it out to actual classes

--- lib/poolparty/modules/searchable_paths.rb
+++ lib/poolparty/modules/searchable_paths.rb
@@ -7,18 +7,23 @@ module PoolParty
     end
 
     module ClassMethods
-      #
+      # Specify that a particular class has methods for searchable paths.
+      # 
       # Options:
       # * <tt>:dirs</tt>: array of directories to look in *under* the search paths. (default: <tt>["/"]</tt>)
       # * <tt>:dir</tt>: set the directory to look in *under* the search paths. Use either dir or dirs, not both. (default: +/+)
+      # * <tt>:paths</tt>: overwrite all default paths and set the paths to this array exactly
+      # * <tt>:append_paths</tt>:  append these paths to any existing paths
+      # * <tt>:prepend_paths</tt>: prepend these paths to any existing paths
       def has_searchable_paths(opts={})
         class_eval do
           extend PoolParty::SearchablePaths::SingletonMethods
 
-          # setup class options
           @searchable_paths_dirs = [opts[:dir]] if opts[:dir]
           @searchable_paths_dirs = opts[:dirs]  if opts[:dirs]
-
+          @paths_override        = opts[:paths] if opts[:paths]
+          @paths_prepend         = opts[:prepend_paths] || []
+          @paths_append          = opts[:append_paths]  || []
         end
         include PoolParty::SearchablePaths::InstanceMethods
       end
@@ -30,34 +35,45 @@ module PoolParty
         @searchable_paths_dirs && @searchable_paths_dirs.size > 0 ? @searchable_paths_dirs : ["/"]
       end
      
-      # Default locations to search for the key
-      # def keypair_paths
-      #   [ 
-      #     "#{ENV["HOME"]}/.ssh",
-      #     "#{Default.poolparty_home_path}/keys",
-      #     PoolParty::Default.base_keypair_path,
-      #     PoolParty::Default.base_config_directory,
-      #     PoolParty::Default.base_ssh_path,
-      #     PoolParty::Default.remote_storage_path,
-      #     Dir.pwd
-      #   ]
-      # end
+      # The default paths are primarily defined in PoolParty::Default. These are the default search paths in order:
+      # 
+      # * current working directory (Dir.pwd)
+      # * ~/.poolparty
+      # * ~/.ec2
+      # * /etc/poolparty
+      # * /var/poolparty
+      def default_paths
+        [
+          Dir.pwd,
+          PoolParty::Default.poolparty_home_path,
+          PoolParty::Default.base_keypair_path,
+          PoolParty::Default.base_config_directory,
+          PoolParty::Default.remote_storage_path
+        ]
+      end
 
+      # returns the full set of valid searchable paths, given the options
+      def searchable_paths
+        return @paths_override if @paths_override && @paths_override.size > 0
+        @paths_prepend + default_paths + @paths_append
+      end
     end
 
     module InstanceMethods
-   # Search for the key in default locations with the entire filepath
-    # if the file exists. If it doesn't exist in the default locations, 
-    # then it returns nil and assumes we it doesn't exist
-    # def search_in_known_locations
-    #   self.class.keypair_paths.each do |path|
-    #     full_path = ::File.join( ::File.expand_path(path), ::File.basename(filepath))
-    #     return full_path if ::File.exists?(full_path)
-    #   end
-    #   raise Exception.new("We cannot continue without a keypair. Please define a keypair in your clouds.rb")
-    #   TODO: Add raise for keypair
-    #   nil
-    # end
+
+      # Search for the file in path locations with the entire filepath
+      # if the file exists. If it doesn't exist in the default locations, 
+      # then it returns nil and assumes we it doesn't exist
+      def search_in_known_locations(filepath)
+        self.class.searchable_paths.each do |path|
+          self.class.searchable_paths_dirs.each do |dir|
+            full_path = File.expand_path(path) / dir / File.basename(filepath)
+            return full_path if File.exists?(full_path)
+          end
+        end
+        nil
+      end
+      alias_method :find_file, :search_in_known_locations
 
     end
 
--- spec/poolparty/modules/searchable_paths_spec.rb
+++ spec/poolparty/modules/searchable_paths_spec.rb
@@ -1,15 +1,17 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-PATH_TEST = Default.storage_directory / "path_test"
+PATH_TEST_ROOT = Default.storage_directory / "path_test"
+PATH_ONE       = PATH_TEST_ROOT / "one"
+PATH_TWO       = PATH_TEST_ROOT / "two"
 
 class TestFile
   include SearchablePaths
-  has_searchable_paths(:dir => "templates")
+  has_searchable_paths(:dir => "templates", :paths => [PATH_ONE, PATH_TWO])
 end
 
 class TestFile2
   include SearchablePaths
-  has_searchable_paths(:dirs => ["clouds", "/"])
+  has_searchable_paths(:dirs => ["clouds", "/"], :prepend_paths => [PATH_ONE, PATH_TWO])
 end
 
 def File.write_to_file(name, content="")
@@ -18,10 +20,20 @@ end
 
 describe "FileWriter" do
   before(:each) do
-    [PATH_TEST/:clouds, PATH_TEST/:templates, PATH_TEST/:foo].each {|dir| FileUtils.mkdir_p(dir) }
-    [PATH_TEST/'clouds.rb', PATH_TEST/:clouds/'special.rb', PATH_TEST/:templates/'apache.conf'].each do |f|
+    # create path one
+    [PATH_ONE/:clouds, PATH_ONE/:templates, PATH_ONE/:foo].each {|dir| FileUtils.mkdir_p(dir) }
+    [PATH_ONE/'clouds.rb', PATH_ONE/:clouds/'special.rb', PATH_ONE/:templates/'apache.conf'].each do |f|
       File.write_to_file(f)
     end
+
+    # create path two
+    [PATH_TWO/:clouds, PATH_TWO/:templates, PATH_TWO/:foo].each {|dir| FileUtils.mkdir_p(dir) }
+    [PATH_TWO/'swing.rb', PATH_TWO/'clouds.rb', PATH_TWO/:clouds/'common.rb', PATH_TWO/:templates/'mysql.conf'].each do |f|
+      File.write_to_file(f)
+    end
+    
+    @template = TestFile.new
+    @cloud    = TestFile2.new
   end
 
   describe "class methods" do
@@ -32,7 +44,33 @@ describe "FileWriter" do
     end
   end
 
+  describe "#find_file" do
+    it "should be able to find a template" do
+      @template.find_file("apache.conf").should_not be_nil
+      @template.find_file("apache.conf").should == PATH_ONE/:templates/'apache.conf'
+      @template.find_file("mysql.conf").should == PATH_TWO/:templates/'mysql.conf'
+    end
+
+    it "shouldn't find a template that doesn't exist" do
+      @template.find_file("post-office.conf").should be_nil
+    end
+
+    it "should find something with prepended paths" do
+      @cloud.find_file("clouds.rb").should == PATH_ONE/'clouds.rb'
+      @cloud.find_file("swing.rb").should == PATH_TWO/'swing.rb'
+    end
+
+    it "should find things in the right order" do
+      @cloud.find_file("clouds.rb").should == PATH_ONE/'clouds.rb'
+    end
+
+
+
+
+
+  end
+
   after(:each) do
-    FileUtils.rm_rf(PATH_TEST)
+    FileUtils.rm_rf(PATH_TEST_ROOT)
   end
 end
