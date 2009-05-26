491de0c55299c9be095e4bd8e1517141928f4d9b
Nate Murray <nate@natemurray.com>
2009-05-26 11:54:11 -0700

    Exception#to_str is removed in Ruby 1.9: http://eigenclass.org/hiki.rb?Changes+in+Ruby+1.9#l53

--- lib/poolparty/core/exception.rb
+++ lib/poolparty/core/exception.rb
@@ -2,7 +2,7 @@
   Exception overloads
 =end
 class Exception
-  alias nice_message to_str
+  alias :nice_message :to_s
   # Gives us a nice_message for exceptions
   def nice_message(padding="")
     "#{padding}#{message}\n#{padding}"# + backtrace.join("\n#{padding}")
