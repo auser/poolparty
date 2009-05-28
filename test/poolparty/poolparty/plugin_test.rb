6fbc08841eddec09124ccff72482477b21302a22
fairchild <fairchild@stimble.net>
2009-05-27 18:44:18 -0700

    change Plugin.available to store an array of constants, not strings. Add a test

--- test/poolparty/poolparty/plugin_test.rb
+++ test/poolparty/poolparty/plugin_test.rb
@@ -7,12 +7,18 @@ end
 
 class TestPlugins < Test::Unit::TestCase
   context "services" do
-    setup do
-      reset!
-      @tbc = TestBaseClass.new do
-        box do
-        end
-      end
+    # setup do
+    #   reset!
+    #   @tbc = TestBaseClass.new do
+    #     box do
+    #     end
+    #   end
+    # end
+    
+    should " have array of availble plugins" do
+      assert Plugin.available.include?(Kernel::BoxClass)
+      assert Plugin.available.include?(Kernel::GitClass)
     end
+    
   end
 end
