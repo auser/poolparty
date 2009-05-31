bd1cc1cf42b7d2c17cfaeca58b1f329ad78d3f20
fairchild <fairchild@stimble.net>
2009-05-30 19:10:05 -0700

    update net/init.rb

--- lib/poolparty/net/init.rb
+++ lib/poolparty/net/init.rb
@@ -5,17 +5,10 @@ class Object
   end
 end
 
-# Load the core net libraries.  These are neccessary for any of the remoter_bases to function.
-%w(remote_instance remoter_base).each do |file|
-  require File.join(::File.dirname(__FILE__),file+'.rb')
-end
-# 
-# # Register available remoter_bases
-# Dir["#{::File.dirname(__FILE__)}/remoter_bases/*/*.rb"].each do |base| 
-#   name = File.join(::File.basename(base, ::File.extname(base)))
-#   require base
-# end
-
+# Load the core net libraries.
+# These are neccessary for any of the remoter_bases to function.
+require ::File.join(::File.dirname(__FILE__),'remoter_base.rb')
+require ::File.join(::File.dirname(__FILE__),'remote_instance.rb')
 
 Dir["#{::File.dirname(__FILE__)}/remoter_bases/*/*.rb"].each do |base|
   name = ::File.basename(base, ::File.extname(base))
