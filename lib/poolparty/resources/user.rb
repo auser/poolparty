190500618e043e377483e1d6445e5d93070b36e8
Nate Murray <nate@natemurray.com>
2009-06-13 03:16:13 +0800

    added ability to set a group for a user on creation
    Signed-off-by: Ari <arilerner@mac.com>

--- lib/poolparty/resources/user.rb
+++ lib/poolparty/resources/user.rb
@@ -6,16 +6,24 @@ module PoolParty
       dsl_methods :name,        # Name of the user
                   :password,    # Password for the user
                   :home,         # Home directory
+                  :group, # primary group for user
                   :authorized_keys
                   
                   
       default_options({
         :shell => "/bin/sh"
       })
+
+      def loaded(o={})
+        @group = dsl_options.delete(:group)
+      end
       
       def after_create
         if authorized_keys
           cloud.has_file :name=>"/home/#{name}/.ssh/authorized_keys", :content => authorized_keys
+        end
+        if @group
+          cloud.has_exec "usermod -g #{@group} #{name}"
         end
       end
       
