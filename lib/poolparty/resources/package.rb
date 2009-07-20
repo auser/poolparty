f422d0458f69b64a6bfe36d9f61bd8d249602164
Ari Lerner <arilerner@mac.com>
2009-07-20 01:31:39 -0700

    Added package resource and corresponding tests

---
+++ lib/poolparty/resources/package.rb
@@ -0,0 +1,29 @@
+module PoolParty
+  module Resources
+    
+    class Package < Resource
+      
+      default_options(
+        :action => :install,
+        :version => nil,
+        :response_file => nil,
+        :source => nil,
+        :options => nil
+      )
+            
+      def print_to_chef        
+        str = <<-EOE
+package "<%= name %>" do
+  action :<%= (action ? action : (exists ? :install : :remove)) %>
+EOE
+        str << "  options <%= print_variable(options) %>\n" if options
+        str << "  version <%= print_variable(version) %>\n" if version
+        str << "  source <%= print_variable(source) %>\n" if source
+        str << "  response_file <%= print_variable(response_file) %>\n" if response_file        
+        str << "end"
+      end
+      
+    end
+    
+  end
+end
---
+++ test/lib/poolparty/resources/package_test.rb
@@ -0,0 +1,28 @@
+require "#{File.dirname(__FILE__)}/../../../test_helper"
+
+include_fixture_resources
+
+class DirectoryResourceTest < Test::Unit::TestCase
+  context "Base" do
+    setup do
+      PoolParty::Resource.define_resource_methods
+      @res = PoolParty::Resources::Package.new "debian-archive-keyring", :options => "--force-yes", :version => "1.16.1-1"
+      @base = PoolParty::DependencyResolvers::Chef
+      @base.compile_directory = test_dir
+    end
+    
+    should "have the method denoted by has_method_name" do
+      str =<<-EOE
+package "debian-archive-keyring" do
+  action :install
+  options "--force-yes"
+  version "1.16.1-1"
+end
+EOE
+
+      assert_equal str.chomp, @base.compile(@res)
+    end
+    
+  end
+  
+end
