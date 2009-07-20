3afdd57b591955b415250df78ca0050f55bd0387
Ari Lerner <arilerner@mac.com>
2009-07-20 01:41:19 -0700

    Fixed exec tests

--- test/lib/poolparty/resources/exec_test.rb
+++ test/lib/poolparty/resources/exec_test.rb
@@ -12,9 +12,9 @@ class ExecResourceTest < Test::Unit::TestCase
       
       @str = <<-EOE
 execute "/usr/sbin/mail -s 'hi' arilerner@mac.com" do
-  command: "/usr/sbin/mail -s 'hi' arilerner@mac.com"
-  path: [ "/usr/bin:/bin:/usr/local/bin:$PATH" ]
-  action: :run
+  command "/usr/sbin/mail -s 'hi' arilerner@mac.com"
+  path [ "/usr/bin:/bin:/usr/local/bin:$PATH" ]
+  action :run
 EOE
     end
     
@@ -24,37 +24,37 @@ EOE
     
     should "have the creates method when it's set" do
       @res.creates = "/etc/poolparty/created"
-      @str << '  creates: "/etc/poolparty/created"'
+      @str << '  creates "/etc/poolparty/created"'
       assert_equal "#{@str}\nend", @res.compile(:chef)
     end
     
     should "have the cwd method when it's set" do
       @res.cwd = "/var/poolparty"
-      @str << '  cwd: "/var/poolparty"'
+      @str << '  cwd "/var/poolparty"'
       assert_equal "#{@str}\nend", @res.compile(:chef)
     end
     
     should "have the environment method when it's set" do
       @res.environment = :development
-      @str << '  environment: :development'
+      @str << '  environment :development'
       assert_equal "#{@str}\nend", @res.compile(:chef)
     end
     
     should "have the group method when it's set" do
       @res.group = 501
-      @str << '  group: 501'
+      @str << '  group 501'
       assert_equal "#{@str}\nend", @res.compile(:chef)
     end
     
     should "have the returns method when it's set" do
       @res.returns = 1
-      @str << '  returns: 1'
+      @str << '  returns 1'
       assert_equal "#{@str}\nend", @res.compile(:chef)
     end
     
     should "have the user method when it's set" do
       @res.user = 502
-      @str << '  user: 502'
+      @str << '  user 502'
       assert_equal "#{@str}\nend", @res.compile(:chef)
     end
   end
