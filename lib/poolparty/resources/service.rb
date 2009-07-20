3f5475e8d4ed72c520f835647e97b5393fc44d82
Ari Lerner <arilerner@mac.com>
2009-07-20 12:00:58 -0700

    Added service resource and corresponding tests

---
+++ lib/poolparty/resources/service.rb
@@ -0,0 +1,38 @@
+module PoolParty
+  module Resources
+    
+    class Service < Resource
+      
+      default_options(
+        :action => nil,
+        :enabled => nil,
+        :running => nil,
+        :pattern => nil,
+        :start_command => nil,
+        :stop_command => nil,
+        :status_command => nil,
+        :restart_command => nil,
+        :reload_command => nil,
+        :supports => nil
+      )
+            
+      def print_to_chef
+str = 'service "<%= name %>" do
+  pattern <%= print_variable(pattern || name) %>
+  action :<%= action ? print_variable(action) : (exists ? :enable : :disable) %>
+'
+        str << "  running <%= print_variable(running) %>\n" if running
+        str << "  start_command <%= print_variable(start_command) %>\n" if start_command
+        str << "  stop_command <%= print_variable(stop_command) %>\n" if stop_command
+        str << "  status_command <%= print_variable(status_command) %>\n" if status_command
+        str << "  restart_command <%= print_variable(restart_command) %>\n" if restart_command
+        str << "  reload_command <%= print_variable(reload_command) %>\n" if reload_command
+        str << "  supports <%= print_variable(supports) %>\n" if supports
+
+        str << "end"
+      end
+      
+    end
+    
+  end
+end
---
+++ test/lib/poolparty/resources/service_test.rb
@@ -0,0 +1,40 @@
+require "#{File.dirname(__FILE__)}/../../../test_helper"
+
+include_fixture_resources
+
+class ServiceResourceTest < Test::Unit::TestCase
+  context "Base" do
+    setup do
+      PoolParty::Resource.define_resource_methods
+      @res = PoolParty::Resources::Service.new "example_service", 
+                                                :action => [:enable, :start],
+                                                :supports => {:status => true, :restart => true, :reload => true}
+      @base = PoolParty::DependencyResolvers::Chef
+      @base.compile_directory = test_dir
+    end
+    
+    should "have the mount method denoted by has_method_name" do
+      str =<<-EOE
+service "example_service" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+EOE
+
+str = <<-EOS
+service "example_service" do
+  pattern "example_service"
+  action :[ :enable, :start ]
+  supports :reload => true,
+:status => true,
+:restart => true
+end
+EOS
+
+
+      assert_equal str.chomp, @base.compile(@res)
+    end
+    
+  end
+  
+end
