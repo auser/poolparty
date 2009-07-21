b24b0721ffeaa393599cdddc8a79f8027286c6f1
Ari Lerner <arilerner@mac.com>
2009-07-21 16:55:51 -0700

    Fixed generic eucalptus sample.rb

--- examples/eucalyptus.rb
+++ examples/eucalyptus.rb
@@ -8,8 +8,6 @@ pool :eucalyptus do
     instances 2
     keypair "eucalyptus-sample"
     using :ec2 do
-      access_key ENV['EC2_ACCESS_KEY']
-      secret_access_key ENV['EC2_SECRET_KEY']
       image_id 'emi-39921602'
     end
   end
