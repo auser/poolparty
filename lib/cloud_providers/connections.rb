a145129fcbd6e8ded9f57bd16116a1e4df4396bc
fairchild <fairchild@stimble.net>
2009-07-22 17:49:40 -0700

    added dns_name to default_options for CloudProvider
    Refactored connections.  
    Added a run command which will be available on all CloudProviderInstances

--- lib/cloud_providers/connections.rb
+++ lib/cloud_providers/connections.rb
@@ -7,16 +7,24 @@ module  CloudProviders
     
     # hostname or ip to use when running remote commands
     def host(n=nil)
-      @host ||= (n.nil? ? public_ip : n)
+      @host ||= (n.nil? ? dns_name : n)
+    end
+    
+    def run(commands, opts={})
+      ssh(commands, opts)
     end
     
     # Simply shell out and call ssh, simple, reliable and fewest dependencies, but slow
-    def ssh( command=[], extra_ssh_ops={})
-      command = command.compact.join(' && ') if command.is_a? Array
-      cmd = "ssh #{host} #{ssh_options(extra_ssh_ops)} '#{command}'"
-      `#{cmd}`
+    def ssh( commands=[], extra_ssh_ops={})
+      commands = commands.compact.join(' && ') if commands.is_a?(Array)
+      cmd_string = "ssh #{host} #{ssh_options(extra_ssh_ops)} '#{commands}'"
+      system_run(cmd_string)
     end
     
+    # Take a hash of options and join them into a string, combined with default options.
+    # Default options are -o StrictHostKeyChecking=no -i keypair.full_filepath -l user
+    # {'-i'=>'keyfile, '-l' => 'fred' } would become
+    # "-i keyfile -o StrictHostKeyChecking=no -i keypair.to_s -l fred"
     def ssh_options(opts={})
       o = {"-i" => keypair.full_filepath,
            "-l" => user,
@@ -25,10 +33,21 @@ module  CloudProviders
       o.collect{ |k,v| "#{k} #{v}"}.join(' ')
     end
     
-    def rsync( source_path, destination_path, rsync_opts=['-v -a'] )
-      dputs "rsync -e 'ssh #{ssh_options}' #{rsync_opts.join(' ')} #{source_path}  root@#{host}:#{destination_path}"
-      out = %x{ rsync -e 'ssh #{ssh_options}' #{rsync_opts.join(' ')} #{source_path}  root@#{host}:#{destination_path} }
+    def rsync( opts={} )
+      raise StandardError.new("You must pass a :source=>uri option to rsync") unless opts[:source]
+      destination_path = opts[:destination] || opts[:source]
+      rsync_opts = opts[:rsync_opts] || '-va'
+      cmd_string =  "rsync -e 'ssh #{ssh_options}' #{rsync_opts} #{opts[:source]}  root@#{host}:#{destination_path}"
+      out = system_run(cmd_string)
       puts out if debugging?
+      out
+    end
+    
+    private
+    # Execute command locally.
+    # This method is mainly broken out to ease testing in the other methods
+    def system_run(cmd)
+      `#{cmd}`
     end
     
   end
--- test/lib/cloud_providers/connections_test.rb
+++ test/lib/cloud_providers/connections_test.rb
@@ -1,10 +1,19 @@
 require "#{File.dirname(__FILE__)}/../../test_helper"
 require File.dirname(__FILE__)+"/cloud_provider_instance_test.rb"
 
-class CloudProviders::CloudProviderInstance
-  def cloud_provider(opts={}, &block)
-    {:keypair=>Keypair.new(fixtures_dir/"keys/test_key")}
+module  CloudProviders
+  module Connections
+    def system_run(cmds, opts={})
+      return cmds
+    end
   end
+  
+  class CloudProviderInstance
+    def cloud_provider(opts={}, &block)
+      {:keypair=>Keypair.new(fixtures_dir/"keys/test_key")}
+    end
+  end
+  
 end
 
 class ConnectionsTest < Test::Unit::TestCase
@@ -12,8 +21,33 @@ class ConnectionsTest < Test::Unit::TestCase
   def inst
     @inst ||= CloudProviders::CloudProviderInstance.new( 
                 :keypair_name => 'test_key',
-                :status=>'pending'
+                :status       => 'pending',
+                :dns_name     => 'bigboy'
               )
+  end
+  
+  def test_default_user
+    assert 'poolparty', inst.user
+    assert 'fred', inst.user('fred')
+  end
+  
+  def test_host
+    assert 'bigboy', inst.host
+    assert inst.dns_name, inst.host
+    assert 'bigsite', inst.host('bigsite')
+  end
+  
+  def test_ssh
+    assert_match /ssh bigboy .* -i (.*)test_key -l poolparty 'uptime'$/, inst.ssh('uptime')
+  end
+  
+  def test_run
+    assert_match /ssh bigboy .* -i (.*)test_key -l poolparty 'uptime'$/, inst.run('uptime')
+    assert_match /ssh bigboy --sshoptions foo .*'uptime'$/, inst.ssh('uptime', {'--sshoptions'=>'foo'})
+  end
+  
+  def test_rsync
+    assert_match /rsync/, inst.rsync(:source=>fixtures_dir/'templates')
   end
   
   def test_ssh_options
