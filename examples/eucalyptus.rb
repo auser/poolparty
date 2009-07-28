da3f34643dc6e1cfd7574b1112dfd16381556458
Ari Lerner <arilerner@mac.com>
2009-07-28 16:35:04 -0700

    Updated cloud loading for binaries\nUpdated eucalpytus example

--- examples/eucalyptus.rb
+++ examples/eucalyptus.rb
@@ -24,12 +24,13 @@ pool :eucalyptus do
     
     has_directory "/etc/poolparty/nodes"
 
-    # clouds.each do |oname, cld|
-      # has_directory "/etc/poolparty/nodes/#{oname}"
-      clouds["bab"].nodes.each do |n|
-        has_file "/etc/poolparty/nodes/bab/#{n.image_id}", :content => "#{n.public_ip}"
+    clouds.each do |oname, cld|
+      has_directory "/etc/poolparty/nodes/#{oname}"
+      cld.nodes.each do |n|
+        p "Adding #{n.instance_id} => #{n.public_ip}"
+        has_file "/etc/poolparty/nodes/#{oname}/#{n.instance_id}", :content => "#{n.public_ip}"
       end
-    # end
+    end
     
   end
   
@@ -44,7 +45,7 @@ pool :eucalyptus do
     clouds.each do |oname, cld|
       has_directory "/etc/poolparty/nodes/#{oname}"
       cld.nodes.each do |n|
-        has_file "/etc/poolparty/nodes/#{oname}/#{n.image_id}", :content => "#{n.public_ip}"
+        has_file "/etc/poolparty/nodes/#{oname}/#{n.instance_id}", :content => "#{n.public_ip}"
       end
     end
     
