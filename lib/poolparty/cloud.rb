ed56a245b38e4d2a9de0a51b017babd259c2bd48
Nate Murray <nate@natemurray.com>
2009-08-27 07:45:24 -0700

    updated cloud to set itself as the nodes cloud

--- lib/poolparty/cloud.rb
+++ lib/poolparty/cloud.rb
@@ -61,7 +61,7 @@ module PoolParty
     end
     
     # Cloud provider methods
-    def nodes(o={}); delayed_action {cloud_provider.nodes(o)}; end
+    def nodes(o={}); delayed_action {cloud_provider.nodes(o).collect{|n| n.cloud = self; n}}; end
     def run_instance(o={}); cloud_provider.run_instance(o);end
     def terminate_instance!(o={}); cloud_provider.terminate_instance!(o);end
     def describe_instances(o={}); cloud_provider.describe_instances(o);end
