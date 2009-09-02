f6ebef4e229ed0887613efd011f2fd6a99376c15
Ari Lerner <arilerner@mac.com>
2009-09-02 16:38:32 -0700

    Updated gemspec again for github

--- config/jeweler.rb
+++ config/jeweler.rb
@@ -26,7 +26,8 @@ end
 
     s.files = (%w(Rakefile README.rdoc License.txt VERSION.yml) + Dir["{config,examples,lib,test,tasks,script,generators,bin,vendor}/**/*"])
     
-    # s.files.exclude 'vendor/erlang/hermes/ebin/*.beam'
+    s.files.exclude 'vendor/erlang/hermes/ebin/*.beam'
+    s.files.exclude 'vendor/erlang/hermes/deps/**/*.beam'
     s.files.exclude 'examples/thrift/**/*.beam'
     # s.files.exclude "**/*/erl_crash.dump"
     
