8d226cc53f028de79f5c557c035c2974395f1c21
fairchild <fairchild@stimble.net>
2009-09-24 00:20:47 -0700

    update collectd plugin

--- lib/poolparty/plugins/collectd.rb
+++ lib/poolparty/plugins/collectd.rb
@@ -1,11 +1,11 @@
+
 module PoolParty
   module Resources
+    FileResource.searchable_paths.unshift(File.dirname(__FILE__)+'/collectd/templates')
   
     class Collectd < Resource
       
-      default_options :server => 'localhost'
-      
-      PoolParty::Resources::FileResource.has_searchable_paths(:prepend_paths=> [File.dirname(__FILE__)+'/collectd/templates'])
+      default_options :server => 'localhost', :base_dir => '/var/lib/collectd'
       
       def after_loaded
         %w(rrdtool 
@@ -17,9 +17,10 @@ module PoolParty
         # has_package librrd-ruby
         # has_gem_package "astro-collectd"
         has_variable 'server', server
+        has_variable 'base_dir', base_dir
         has_file '/etc/collectd/collectd.conf' do
           template 'collectd.conf.erb'
-          notifies get_service('collectd'), :restart
+          # notifies get_service('collectd'), :restart
         end
       end
       
