8334edecfd1a1cb763de202ea82d73161d350eca
Ari Lerner <arilerner@mac.com>
2009-10-06 17:57:45 -0700

    Renamed cidr_ip to network in authorize/revoke for security groups

--- lib/cloud_providers/ec2/helpers/authorize.rb
+++ lib/cloud_providers/ec2/helpers/authorize.rb
@@ -4,14 +4,14 @@ module CloudProviders
                 :protocol => "tcp",
                 :from_port => "22",
                 :to_port => "22",
-                :cidr_ip => "0.0.0.0/0"})
+                :network => "0.0.0.0/0"})
     def run
-      puts "Authorizing: #{cloud.proper_name} for #{protocol} to #{from_port}:#{to_port} #{cidr_ip}"
+      puts "Authorizing: #{cloud.proper_name} for #{protocol} to #{from_port}:#{to_port} #{network}"
       options = { :group_name => cloud.proper_name,
                   :ip_protocol => protocol,
                   :from_port => from_port,
                   :to_port => to_port,
-                  :cidr_ip => cidr_ip}
+                  :cidr_ip => network}
               
       ec2.authorize_security_group_ingress(options) rescue nil
     end
--- lib/cloud_providers/ec2/helpers/revoke.rb
+++ lib/cloud_providers/ec2/helpers/revoke.rb
@@ -5,14 +5,14 @@ module CloudProviders
                 :protocol => "tcp",
                 :from_port => "22",
                 :to_port => "22",
-                :cidr_ip => "0.0.0.0/0"})
+                :network => "0.0.0.0/0"})
     def run
-      puts "Revoking: #{cloud.proper_name} for #{protocol} to #{from_port}:#{to_port} #{cidr_ip}"
+      puts "Revoking: #{cloud.proper_name} for #{protocol} to #{from_port}:#{to_port} #{network}"
       options = { :group_name => proper_name,
                   :ip_protocol => protocol,
                   :from_port => from_port,
                   :to_port => to_port,
-                  :cidr_ip => cidr_ip}
+                  :cidr_ip => network}
                 
       ec2.revoke_security_group_ingress(options) rescue nil
     end
