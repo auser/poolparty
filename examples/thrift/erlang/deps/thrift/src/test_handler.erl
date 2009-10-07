300b63ec4f57eb6f42dc37bd5bd4cecfb8672f3c
Ari Lerner <arilerner@mac.com>
2009-10-06 17:50:48 -0700

    Removed thrift from examples

--- examples/thrift/erlang/deps/thrift/src/test_handler.erl
+++
@@ -1,7 +0,0 @@
--module(test_handler).
-
--export([handle_function/2]).
-
-handle_function(add, Params = {A, B}) ->
-    io:format("Got params: ~p~n", [Params]),
-    {reply, A + B}.
--- examples/thrift/erlang/include/commandInterface_thrift.hrl
+++
@@ -1,5 +0,0 @@
--ifndef(_commandInterface_included).
--define(_commandInterface_included, yeah).
--include("poolparty_types.hrl").
-
--endif.
