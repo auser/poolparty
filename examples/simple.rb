a1464417890425e54e06e92dd908c73219dc70dd
Ari Lerner <arilerner@mac.com>
2009-10-28 12:10:33 -0700

    Fixed failing custom security group name specified

--- examples/simple.rb
+++ examples/simple.rb
@@ -9,6 +9,12 @@ pool "poolparty" do
     instances 1
     image_id "ami-ccf615a5" #alestic jaunty
     availability_zones ['us-east-1c']
+    
+    security_group(:name => 'start_test') do
+      authorize :from_port => '22', :to_port => '22'
+      authorize :from_port => '80', :to_port => '80'
+    end
+    
   end
   
 end
